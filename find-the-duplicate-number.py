820c7da70b789c610c42f20a05c4630f8f8ef140
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2020-11-29 03:05:46 +0530

    working commit - Created using LeetHub

---
+++ find-the-duplicate-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        hash_set = {}
+        for i in nums:
+            if i not in hash_set:
+                hash_set[i] = i
+            else:
+                return i
