3a40160d1e76a50916c96a12616cc0e6ac7c52e0
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2020-12-20 02:25:52 +0530

    Time: 84 ms (35.96%), Space: 17.5 MB (16.57%) - LeetHub

--- insert-interval/insert-interval.py
+++ insert-interval/insert-interval.py
@@ -1,9 +1,10 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        
+        
         intervals.append(newInterval)
-        
         intervals.sort(key = lambda x: x[0])
-        
         i = 0
         while i < len(intervals) - 1:
             if intervals[i][1] >= intervals[i + 1][0]:
