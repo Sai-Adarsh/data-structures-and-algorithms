1785e55b739cd6280afbcb0ea70d35ac08e52afc
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2020-12-26 01:20:24 +0530

    Time: 104 ms (9.53%), Space: 14.9 MB (8.31%) - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -1,17 +1,11 @@
-​
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        import collections
         ransomNote = [x for x in ransomNote]
         magazine = [x for x in magazine]
-        for i in range(0, len(ransomNote)):
+        for i in ransomNote:
             try:
-                indexing = magazine.index(ransomNote[i])
-                magazine[indexing] = None
+                magazine[magazine.index(i)] = None
             except:
                 continue
-        count = magazine.count(None)
-        if len(ransomNote) == count:
-            return True
-        else:
-            return False    
+            
+        return len(ransomNote) == magazine.count(None)
