fe6c4f4d2c9503c45f9e3fbc74db8c799543707a
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-01-10 02:34:46 +0530

    Time: 88 ms (84.88%), Space: 17.8 MB (60.16%) - LeetHub

---
+++ deepest-leaves-sum/deepest-leaves-sum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        if not root:
+            return
+        from collections import deque
+        q = deque([root])
+        curr_level = []
+        all_levels = []
+        
+        while q:
+            curr_level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                curr_level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            all_levels.append(curr_level)
+        return sum(all_levels[-1])
