df360c4ac09296aa6f540e8b38627e83d62b018f
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-01-11 02:54:21 +0530

    Time: 32 ms (54.51%), Space: 14.2 MB (68.39%) - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if not head:
+            return
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
