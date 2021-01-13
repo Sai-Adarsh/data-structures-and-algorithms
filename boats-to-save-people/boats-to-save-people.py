a524560d9ed0000e29cb07ff55b333040d4058b7
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-01-14 03:50:03 +0530

    Time: 424 ms (99.62%), Space: 21.1 MB (11.12%) - LeetHub

---
+++ boats-to-save-people/boats-to-save-people.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i, j = 0, len(people) - 1
+        ans = 0
+        while i <= j:
+            ans += 1
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+        return(ans)
