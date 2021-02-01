01ca972e32ebf74d7932b0df006655f4c017c693
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2021-02-01 07:10:15 +0530

    CPP

--- CPP/Data Structures/sum.cpp
+++ CPP/Data Structures/sum.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <bits/stdc++.h>
 #include <algorithm>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution {
-	public:
-		int sumOfArrays(vector <int> arr) {
-			int res = 0;
-			for (int i = 0; i < arr.size(); i++, res += arr[i]);
-			return res;
-		}
-};
-
 int main() {
-	Solution Obj;
-	vector <int> arr = {1, 2, 3, 4, 5};
- 	cout<<Obj.sumOfArrays(arr);
+  vector <int> result = {1, 2, 3, 4};
+  int res = 0;
+  for (int i = 0; i < 4; i++) {
+    res += result[i];
+  }
+  cout<<"Im here: "<<res;
 }
