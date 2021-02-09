f6e52f9067412bb508be70bc9f3834b78d5625a5
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-02-10 05:03:49 +0530

    Time: 68 ms (91.02%), Space: 14.5 MB (8.34%) - LeetHub

--- max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
+++ max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        from numpy import transpose
         presum = sum([sum(i) for i in grid])
         rowMax = [max(i) for i in grid]
-        colMax = [max(i) for i in transpose(grid)]
+        colMax = [max(i) for i in zip(*grid)]
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 grid[i][j] = min(rowMax[i], colMax[j])
