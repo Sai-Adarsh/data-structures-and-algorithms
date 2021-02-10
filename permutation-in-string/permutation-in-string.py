007bc818607fe8bb868919951785fec5c97cb7d8
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-02-11 02:29:55 +0530

    Time: 6592 ms (5.00%), Space: 14.1 MB (96.01%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -1,6 +1,6 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        for i in range(len(s2) - len(s1) + 1):
-            if sorted(s1) == sorted(s2[i:i + len(s1)]):
-                return True
-        return False
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        for i in range(len(s2) - len(s1) + 1):
+            if sorted(s2[i:i + len(s1)]) == sorted(s1):
+                return True
+        return False
