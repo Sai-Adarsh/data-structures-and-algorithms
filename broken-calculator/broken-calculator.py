5a41910130d727de30662208710a0ceab8c39e91
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-02-24 22:47:01 +0530

    Time: 32 ms (57.14%), Space: 14.3 MB (14.95%) - LeetHub

--- broken-calculator/broken-calculator.py
+++ broken-calculator/broken-calculator.py
@@ -2,11 +2,13 @@ class Solution:
     def brokenCalc(self, X: int, Y: int) -> int:
         if X > Y:
             return X - Y
-        res = 0
+        result = 0
         while Y > X:
-            res += 1
+            result += 1
             if Y % 2 == 0:
                 Y //= 2
             else:
                 Y += 1
-        return res + (X - Y)
+        return result + (X - Y)
+                
+        
