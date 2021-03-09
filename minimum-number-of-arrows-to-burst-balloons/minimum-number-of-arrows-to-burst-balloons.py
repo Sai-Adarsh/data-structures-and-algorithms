63c16219d3b431d2a0bfaca5508f276359e9153c
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-09 20:23:14 +0530

    Time: 420 ms (60.02%), Space: 19.5 MB (47.44%) - LeetHub

--- minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -3,15 +3,13 @@ class Solution:
         if not points:
             return 0
         count = 1
-        
         points.sort(key = lambda x: x[1])
         left = 0
         right = 1
         
         while right < len(points):
             if points[left][1] < points[right][0]:
-                count += 1
                 left = right
+                count += 1
             right += 1
-        
         return count
