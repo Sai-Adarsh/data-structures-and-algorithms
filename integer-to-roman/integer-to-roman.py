816ea6742b710081c94d2b6652c5414aeebc3a04
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-11 23:55:29 +0530

    Time: 48 ms (75.14%), Space: 14.4 MB (31.02%) - LeetHub

--- integer-to-roman/integer-to-roman.py
+++ integer-to-roman/integer-to-roman.py
@@ -1,21 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        def printRoman(number):
-            num = [1, 4, 5, 9, 10, 40, 50, 90, 
-                   100, 400, 500, 900, 1000]
-            sym = ["I", "IV", "V", "IX", "X", "XL", 
-                   "L", "XC", "C", "CD", "D", "CM", "M"]
-            i = 12
-            res = ""
-            while number:
-                div = number // num[i]
-                number %= num[i]
-
-                while div:
-                    res += sym[i]
-                    div -= 1
-                i -= 1
-            return res
- 
-        number = num
-        return printRoman(number)
+        num_list = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+        sym_list = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+        
+        i = 12
+        res = ""
+        
+        while num:
+            quotient = num // num_list[i] # q = 3549 // 1000 = 3
+            num = num % num_list[i]
+            
+            res += quotient * sym_list[i]
+            i -= 1
+        
+        
+        return res
