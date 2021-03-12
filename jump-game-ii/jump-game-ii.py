6acbe2dcd1175397b2392448e348558b0257b8d1
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-12 23:40:44 +0530

    Time: 28 ms (100.00%), Space: 14.2 MB (100.00%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        import sys
+        dp = [sys.maxsize for _ in range(len(nums) - 1)] + [0]
+        from collections import deque
+        for i in range(len(nums) - 2, - 1, -1):
+            try:
+                dp[i] = 1+ min(dp[i + 1:i + nums[i] + 1])
+            except:
+                dp[i] = sys.maxsize
+        return dp[0]
