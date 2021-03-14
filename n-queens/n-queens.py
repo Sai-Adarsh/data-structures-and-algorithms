1bee4ad1ba6e22a50dfa2289cd20ac3a3977ad86
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-14 22:26:02 +0530

    Time: 56 ms (86.05%), Space: 14.3 MB (99.51%) - LeetHub

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,16 +1,15 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-    
+        L = self.backTracking([], [], [], [], n, 0)
         
-        def DFS(queens, diagonal_one, diagonal_two, row):
-            if row == n:
-                self.result.append(queens)
-            else:
-                for col in range(n):
-                    if col not in queens and row - col not in diagonal_one and row + col not in diagonal_two:
-                        DFS(queens + [col], diagonal_one + [row - col], diagonal_two + [row + col], row + 1)
-            
-        self.result = []
-        DFS([], [], [], 0)
-        return [["".join(['Q' if _ == com else "." for _ in range(n)]) for com in arr] for arr in self.result]
-    
+        return [["".join(['Q' if _ == com else "." for _ in range(n)]) for com in arr] for arr in L]
+        
+    def backTracking(self, result, queens, diagonal_one, diagonal_two, n, row):
+        if row == n:
+            result.append(queens)
+        
+        for col in range(n):
+            if col not in queens and row - col not in diagonal_one and row + col not in diagonal_two:
+                self.backTracking(result, queens + [col], diagonal_one + [row - col], diagonal_two + [row + col], n, row + 1)
+        
+        return result
