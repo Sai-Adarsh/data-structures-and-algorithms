e7188bf9eb483a654f54100e0401bb9965d3cc90
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-18 22:36:11 +0530

    Time: 484 ms (78.18%), Space: 49.5 MB (58.36%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,26 +1,30 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        from collections import deque
-        q = deque([root])
-        curr_level = []
-        all_levels = []
-        found = False
-        while q:
-            curr_level = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                curr_level.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                if not node.right and not node.left:
-                    found = True
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        from collections import deque
+        q = deque([root])
+        curr_level = []
+        all_levels = []
+        met = False
+        while q:
+            curr_level = []
+            
+            for _ in range(len(q)):
+                node = q.popleft()
+                curr_level.append(node.val)
+                if not node.left and not node.right:
+                    met = True
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            all_levels.append(curr_level)
+            if met == True:
+                return len(all_levels)
