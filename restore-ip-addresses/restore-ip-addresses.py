17fa851e21724519df28886b719e260b1873d6a5
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-21 00:04:00 +0530

    Time: 20 ms (99.90%), Space: 14.3 MB (67.18%) - LeetHub

--- restore-ip-addresses/restore-ip-addresses.py
+++ restore-ip-addresses/restore-ip-addresses.py
@@ -1,21 +1,24 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         
-        return self.backTracking([], s, [], 0)
-        return self.res
+        return self.backTracking([], [], 0, s)
         
-    def backTracking(self, res, s, curr_path, step):
+    def backTracking(self, res, curr_path, address_count, s):
+        
         # our goal
-        if step == 4 and not s:
+        # base case
+        if address_count == 4 and (len(s) == 0 or not s):
             res.append(".".join(curr_path))
             return
         
-        if len(s) > (4 - step) * 3 or len(s) < (4 - step):
+        if len(s) > (4 - address_count) * 3 or len(s) < (4 - address_count):
             return
+        # our constraint
+        for splitter in range(min(3 if s[0] != "0" else 1, len(s))):
+            sub_string = s[:splitter + 1]
+            if int(sub_string) < 256:
+                # our choice:
+                self.backTracking(res, curr_path + [s[: splitter + 1]], address_count + 1, s[splitter + 1 :])
         
-        for i in range(min(3 if s[0] != '0' else 1 , len(s))):
-            # our constraint
-            if int(s[:i + 1]) < 256: 
-            #our choice
-                self.backTracking(res, s[i + 1:], curr_path + [s[:i + 1]], step + 1)
+        
         return res
