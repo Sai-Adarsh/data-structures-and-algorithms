4f255fa1a320ef3b1bb55a8eddcd37b63c53399b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-21 20:09:49 +0530

    Time: 7048 ms (5.24%), Space: 40.1 MB (15.73%) - LeetHub

--- reordered-power-of-2/reordered-power-of-2.py
+++ reordered-power-of-2/reordered-power-of-2.py
@@ -1,6 +1,8 @@
 class Solution:
     def reorderedPowerOf2(self, N: int) -> bool:
+        # how is a number converted to binary
+        # all powers of two's binary has ones at only one place
         from itertools import permutations
-        L = list(map(list, permutations(str(N))))
-        L = ["".join(i) for i in L if i[0] != "0" and bin(int("".join(i))).count("1") == 1]
+        L = list(map("".join, permutations(str(N))))
+        L = ["".join(i) for i in L if "".join(i)[0] != "0" and bin(int("".join(i))).count("1") == 1]
         return len(L) >= 1
