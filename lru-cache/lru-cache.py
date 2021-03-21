4e5205cdd6e387b0cf333fbedad481d3b3000003
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-21 22:10:59 +0530

    Time: 180 ms (74.55%), Space: 23.5 MB (76.03%) - LeetHub

--- lru-cache/lru-cache.py
+++ lru-cache/lru-cache.py
@@ -1,24 +1,24 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.hash_map = OrderedDict()
+        self.cache = OrderedDict()
         self.capacity = capacity
-
+        
     def get(self, key: int) -> int:
-        if key not in self.hash_map:
+        if key not in self.cache:
             return -1
-        found = self.hash_map.pop(key)
-        self.hash_map[key] = found
+        found = self.cache[key]
+        del self.cache[key]
+        self.cache[key] = found
         return found
 
     def put(self, key: int, value: int) -> None:
-        
-        if key in self.hash_map:
-            found = self.hash_map.pop(key)
-        elif len(self.hash_map) >= self.capacity:
-            self.hash_map.popitem(last = False)
-        self.hash_map[key] = value
-        
+        if key in self.cache:
+            del self.cache[key]
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last = False)
+        self.cache[key] = value
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
