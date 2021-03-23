efffbb675f2a6baa6d91a8c20f0888c3f0816eb8
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-23 19:46:06 +0530

    Time: 32 ms (77.82%), Space: 14.1 MB (76.43%) - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -3,4 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        zero = []
+        one = []
+        two = []
+        
+        for i in nums:
+            if i == 0:
+                zero.append(0)
+            elif i == 1:
+                one.append(1)
+            else:
+                two.append(2)
+                
+        nums[:] = zero + one + two
