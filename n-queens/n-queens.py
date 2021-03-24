596e773dc475fca2c92064dd963c1a972abd3025
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-24 22:32:53 +0530

    Time: 56 ms (86.05%), Space: 14.7 MB (54.28%) - LeetHub

--- n-queens/n-queens.py
+++ n-queens/n-queens.py
@@ -1,21 +1,21 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         
-        L = self.backTracking([], [], [], [], 0, n)
-        return [["".join(['Q' if _ == queen_at else '.' for _ in range(n)]) for queen_at in arr] for arr in L]
-        
-    def backTracking(self, res, queen, diagonal_one, diagonal_two, row, n):
+        L = self.backTracking([], [], [], [], n, 0)
+        return [["".join(['Q' if _ == where_queen else "." for _ in range(n)]) for where_queen in arr] for arr in L]
+    
+    def backTracking(self, res, queens, diagonal_one, diagonal_two, n, row):
         # our goal
         # base case
         if row == n:
-            res.append(queen)
+            res.append(queens)
+            return
         
-        # our constraint
+        # our constraints
+        # recursive case
         for col in range(n):
-            if col not in queen and row - col not in diagonal_one and row + col not in diagonal_two:
-                # our choice
-                self.backTracking(res, queen + [col], diagonal_one + [row - col], diagonal_two + [row + col], row + 1, n)
-        
+            if col not in queens and row - col not in diagonal_one and row + col not in diagonal_two:
+                self.backTracking(res, queens + [col], diagonal_one + [row - col], diagonal_two + [row + col], n, row + 1)
+                
         return res
-        
-        
+    
