40cb97c63cad0478ffa2996b2d4e236c57e3b256
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-26 23:27:25 +0530

    Time: 76 ms (63.59%), Space: 14.4 MB (78.31%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,24 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         
-        def DFS(image, i, j, curr_color, newColor):
-            #base case
-            
+        def DFS(image, i, j, curr_color):
+            # base case
             q = []
-            q.append((i, j))
+            q.append([i, j])
+            visited = set()
             visited.add((i, j))
             
-            #recursive case
+            # recursive case
             while q:
                 x, y = q.pop()
                 image[x][y] = newColor
-                neighbours = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                neighbours = ((0, 1), (0, -1), (1, 0), (-1, 0))
                 for dx, dy in neighbours:
-                    if 0 <= x + dx <= len(image) - 1 and 0 <= y + dy <= len(image[0]) - 1 and tuple((x + dx, y + dy)) not in visited and image[x + dx][y + dy] == curr_color:
-                        q.append((x + dx, y + dy))
-                        visited.add((x + dx, y + dy))
+                    if 0 <= x + dx <= len(image) - 1  and 0 <= y + dy <= len(image[0]) - 1 and tuple((x + dx, y + dy)) not in visited and image[x + dx][y + dy] == curr_color:
+                        q.append([x + dx, y + dy])
+                        visited.add(tuple((x + dx, y + dy)))
             return image
             
-        visited = set()
-        return DFS(image, sr, sc, image[sr][sc], newColor)
-                    
+        
+        
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if i == sr and j == sc:
+                    return DFS(image, i, j, image[i][j])
