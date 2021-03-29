7192f00f644f6bba9e8d78725fdc47b15869241a
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-29 22:35:56 +0530

    Time: 48 ms (46.94%), Space: 19.6 MB (10.78%) - LeetHub

--- path-sum-ii/path-sum-ii.py
+++ path-sum-ii/path-sum-ii.py
@@ -1,20 +1,19 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
-        
-        L = []
-        from numpy import sum as summation
-        def DFS(root, s):
-            if not root:
-                return
-            if not root.left and not root.right and summation(s + [root.val]) == sum:
-                L.append(s + [root.val])
-            DFS(root.left, s + [root.val])
-            DFS(root.right, s + [root.val])
-        DFS(root, [])
-        return L
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        L = []
+        s = []
+        def DFS(root, L, s):
+            if not root:
+                return
+            if not root.left and not root.right and sum(s + [root.val]) == targetSum:
+                L.append(s + [root.val])
+            DFS(root.left, L, s + [root.val])
+            DFS(root.right, L, s + [root.val])
+        DFS(root, L, s)
+        return L
