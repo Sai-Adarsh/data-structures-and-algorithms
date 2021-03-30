28bbcc71b70c02d70cdb9d0b2d47f72cec147385
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-03-30 23:27:44 +0530

    Time: 164 ms (72.40%), Space: 15.5 MB (79.09%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,9 +1,7 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        L = Counter(nums)
-        print(L)
-        n = len(nums)
-        for i in L:
-            if L[i] > n/2:
-                return i
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        from collections import Counter
+        L = list(map(list, Counter(nums).items()))
+        L = list(filter(lambda x: x[1] > len(nums) // 2, L))
+        return L[0][0]
