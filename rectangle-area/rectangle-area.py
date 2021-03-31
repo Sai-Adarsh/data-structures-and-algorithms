79b9915bf09c1c53b9c00e083f4f2c64345e97fa
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-01 00:30:25 +0530

    Time: 56 ms (47.00%), Space: 14.5 MB (23.00%) - LeetHub

--- rectangle-area/rectangle-area.py
+++ rectangle-area/rectangle-area.py
@@ -1,11 +1,11 @@
 class Solution:
     def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        
+        overlap = abs(min(C, G) - max(A, E)) * abs(min(D, H) - max(B, F))
         area1 = (C - A) * (D - B)
         area2 = (G - E) * (H - F)
         
-        overlap = 0
-        if C <= E or A >= G or D <= F or B >= H: 
+        if C <= E or D <= F or G <= A or H <= B:
             overlap = 0
-        else:
-            overlap =  (min(C, G) - max(A, E)) * (min(D, H) - max(B, F))
+        
         return area1 + area2 - overlap
