401f76a5b93ce4b99021c4574871d47af7b562bf
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-01 10:39:42 +0530

    Time: 28 ms (95.73%), Space: 14.1 MB (87.73%) - LeetHub

--- restore-ip-addresses/restore-ip-addresses.py
+++ restore-ip-addresses/restore-ip-addresses.py
@@ -4,22 +4,21 @@ class Solution:
         return self.backTracking([], [], s, 0)
         
     def backTracking(self, res, curr_path, s, start):
-        # our goal
+        # our solution
         # base case
         if start == 4 and not s:
             res.append(".".join(curr_path))
             return
         
-        # our constraints
-        # recursive case
         if len(s) > (4 - start) * 3 or len(s) < (4 - start):
             return
-        
-        # our choice
+        # our constraints
+        # recursive case
         for i in range(min(3 if s[0] != "0" else 1, len(s))):
-            sub_sting = s[:i + 1]
-            if int(sub_sting) < 256:
-                self.backTracking(res, curr_path + [sub_sting], s[i + 1:], start + 1)
+            sub_string = s[:i + 1]
+            if int(sub_string) < 256:
+                self.backTracking(res, curr_path + [sub_string], s[i + 1:], start + 1)
+        
         
         return res
         
