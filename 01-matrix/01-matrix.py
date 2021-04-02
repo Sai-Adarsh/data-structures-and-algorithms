dfcdb15267c5c6958f10fd7cb78f35a567818b05
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-02 17:46:24 +0530

    Time: 640 ms (78.17%), Space: 17.2 MB (47.46%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -1,6 +1,7 @@
 class Solution:
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         
+        
         import sys
         dp = [[sys.maxsize - 10000 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
         
@@ -13,7 +14,7 @@ class Solution:
                         dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1)
                     if j > 0:
                         dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1)
-                        
+        
         for i in range(len(matrix) - 1, -1, -1):
             for j in range(len(matrix[0]) - 1, -1, -1):
                 if i < len(matrix) - 1:
