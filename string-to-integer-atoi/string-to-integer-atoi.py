9cd2c8261581ce5a56c79167954bb597955a8bb6
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-02 17:57:50 +0530

    Time: 16 ms (99.96%), Space: 14.3 MB (25.38%) - LeetHub

---
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,5 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        import re
+        e = re.search(r"^[-+]?\d+", s.lstrip())
+        return max(min(int(e.group(0)), 2**31-1), -2**31) if e else 0
