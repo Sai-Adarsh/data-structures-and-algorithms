d031af76fa4ed80ccc2ae07c4fde95730e4f8e7d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-03 15:02:13 +0530

    Time: 748 ms (20.73%), Space: 14.4 MB (72.51%) - LeetHub

--- sudoku-solver/sudoku-solver.py
+++ sudoku-solver/sudoku-solver.py
@@ -1,11 +1,11 @@
 class Solution:
     def helper(self, board):
-        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == ".":
                     for num in range(1, 10):
                         num = str(num)
+                        
                         if self.isValid(board, i, j, num):
                             board[i][j] = num
                             if self.helper(board):
@@ -16,9 +16,9 @@ class Solution:
     
     def isValid(self, board, row, col, num):
         for i in range(9):
-            if board[i][col] == num:
-                return False
             if board[row][i] == num:
+                return False
+            if board[i][col] == num:
                 return False
             if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
                 return False
