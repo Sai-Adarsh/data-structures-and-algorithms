a051ca3eeb5bd775987096a5b2d2e83c006cc3f0
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-03 23:37:47 +0530

    Time: 32 ms (91.15%), Space: 14.2 MB (84.80%) - LeetHub

---
+++ rotate-list/rotate-list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        
+        if not head:
+            return
+        
+        from collections import deque
+        L = deque()
+        
+        root = head
+        while root:
+            if not root:
+                return
+            L.append(root)
+            root = root.next
+        
+        for i in range(k % len(L)):
+            L.appendleft(L.pop())
+            
+        root = head = L.popleft()
+        
+        while L:
+            root.next = L.popleft()
+            root = root.next
+        
+        root.next = None
+        return head
