7298d0894432bee25009fdd2c03124917c17de75
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-05 10:02:09 +0530

    Time: 32 ms (59.13%), Space: 14.2 MB (65.42%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -7,9 +7,10 @@ class Solution:
         for i in s:
             stack.append(i)
             if len(stack) >= 2:
-                if (stack[-1] == "}" and stack[-2] == "{") or (stack[-1] == ")" and stack[-2] == "(") or (stack[-1] == "]" and stack[-2] == "["):
+                if (stack[-1] == ")" and stack[-2] == "(") or (stack[-1] == "]" and stack[-2] == "[") or (stack[-1] == "}" and stack[-2] == "{"):
                     stack.pop()
                     stack.pop()
                     
                     
         return not stack
+                    
