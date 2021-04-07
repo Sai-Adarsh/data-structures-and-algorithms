be80ea8b7f0b99abb4bb9d12d87005c0ee4d3457
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-07 21:14:34 +0530

    Time: 56 ms (13.74%), Space: 15.8 MB (7.12%) - LeetHub

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,27 +1,29 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        if not head:
-            return
-        L = []
-        while head:
-            if not head:
-                return
-            L.append(head.val)
-            head = head.next
-        i = 0
-        while i <= len(L) - k:
-            L[i:k + i] = L[i:k + i][::-1]
-            i += k
-        first = ListNode(L[0])
-        final = first
-        
-        for i in range(1, len(L)):
-            node = ListNode(L[i])
-            first.next = node
-            first = node
-        return final
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if not head:
+            return
+        L = []
+        
+        while head:
+            if not head:
+                return
+            L.append(head.val)
+            head = head.next
+        i = 0
+        while i <= len(L) - k:
+            L[i:i + k] = L[i:i + k][::-1]
+            i += k
+                        
+        first = ListNode(L[0])
+        final = first
+        
+        for _ in range(1, len(L)):
+            node = ListNode(L[_])
+            first.next = node
+            first = node
+        return final
