4bee112fd303d02b644f24096715b17240ab0557
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-08 23:25:53 +0530

    Time: 40 ms (47.48%), Space: 15.2 MB (48.27%) - LeetHub

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -12,17 +12,19 @@ class Solution:
         if not root:
             return
         self.L = []
+        
         def DFS(root):
             if not root:
                 return
             self.L.append(root)
             DFS(root.left)
             DFS(root.right)
-            
         DFS(root)
+        
         for i in range(len(self.L)):
             if i == len(self.L) - 1:
                 self.L[i].left, self.L[i].right = None, None
             else:
                 self.L[i].left, self.L[i].right = None, self.L[i + 1]
-            
+        return self.L[0]
+                
