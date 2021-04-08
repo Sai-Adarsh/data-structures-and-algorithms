364a824008fd9817a20b1107ca5f4286b4e8f891
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-08 23:02:11 +0530

    Time: 36 ms (44.83%), Space: 14.2 MB (77.69%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,29 +1,30 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        if not head:
-            return
-        L = []
-        curr = head
-        while curr:
-            if not curr:
-                return
-            L.append(curr.val)
-            curr = curr.next
-        print(L)
-        del L[len(L) - n]
-        
-        if not L:
-            return
-        first = ListNode(L[0])
-        final = first
-        
-        for i in range(1, len(L)):
-            node = ListNode(L[i])
-            first.next = node
-            first = node
-        return final
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head:
+            return
+        
+        count = 0
+        
+        root = head
+        while root:
+            if not root:
+                return
+            count += 1
+            root = root.next
+            
+        n = count - n
+        count2 = 0
+        root = head
+        while root:
+            if count == count - n:
+                return root.next
+            if count2 + 1 == n:
+                root.next = root.next.next
+            count2 += 1
+            root = root.next
+        return head
