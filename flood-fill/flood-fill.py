cbfbfe0560e9ef9f97d7d865618604aaea085a52
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-10 21:04:51 +0530

    Time: 84 ms (6.65%), Space: 14.3 MB (76.86%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,27 +1,30 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         
-        def DFS(image, i, j, curr_color):
+        
+        
+        def DFS(image, i, j, newColor, currColor):
+            # our solution
             # base case
             q = []
             q.append([i, j])
             visited = set()
             visited.add((i, j))
             
+            # our constraints
             # recursive case
             while q:
                 x, y = q.pop()
                 image[x][y] = newColor
-                neighbours = ((0, 1), (0, -1), (1, 0), (-1, 0))
+                neighbours = ((1, 0), (-1, 0), (0, 1), (0, -1))
                 for dx, dy in neighbours:
-                    if 0 <= x + dx <= len(image) - 1  and 0 <= y + dy <= len(image[0]) - 1 and tuple((x + dx, y + dy)) not in visited and image[x + dx][y + dy] == curr_color:
-                        q.append([x + dx, y + dy])
+                    if 0 <= x + dx <= len(image) - 1 and 0 <= y + dy <= len(image[0]) - 1 and image[x + dx][y + dy] == currColor and tuple((x + dx, y + dy)) not in visited:
+                        
                         visited.add(tuple((x + dx, y + dy)))
+                        q.append([x + dx, y + dy])
             return image
-            
-        
         
         for i in range(len(image)):
             for j in range(len(image[0])):
                 if i == sr and j == sc:
-                    return DFS(image, i, j, image[i][j])
+                    return DFS(image, i, j, newColor, image[sr][sc])
