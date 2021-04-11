72fba70f2b0a1485b4bde6d8a5d98cbae23f5850
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-11 21:22:52 +0530

    Time: 48 ms (82.70%), Space: 15.2 MB (82.70%) - LeetHub

--- path-sum-iii/path-sum-iii.py
+++ path-sum-iii/path-sum-iii.py
@@ -5,23 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: TreeNode, sum: int) -> int:
-        
+    def pathSum(self, root: TreeNode, targetSum: int) -> int:
+
         def DFS(root, prev_sum):
             if not root:
                 return
             curr_sum = prev_sum + root.val
-            if curr_sum - sum in prefix_sum:
-                self.count += prefix_sum[curr_sum - sum]
+            if curr_sum - targetSum in prefix_sum:
+                self.count += prefix_sum[curr_sum - targetSum]
             if curr_sum not in prefix_sum:
                 prefix_sum[curr_sum] = 1
             else:
                 prefix_sum[curr_sum] += 1
-            
             DFS(root.left, curr_sum)
             DFS(root.right, curr_sum)
             prefix_sum[curr_sum] -= 1
-            
             
         self.count = 0
         prefix_sum = {0: 1}
