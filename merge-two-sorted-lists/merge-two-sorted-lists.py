3e00a11ba50b1f1cee4f7e51eeb16bad9d142b4d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-17 15:27:56 +0530

    Time: 28 ms (97.76%), Space: 14.2 MB (62.49%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,27 +1,33 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1 and not l2:
-            return
-        L = []
-        if l1:
-            while l1:
-                L.append(l1.val)
-                l1 = l1.next
-        if l2:
-            while l2:
-                L.append(l2.val)
-                l2 = l2.next
-        L.sort()
-        first = ListNode(L[0])
-        final = first
-        for i in range(1, len(L)):
-            node = ListNode(L[i])
-            first.next = node
-            first = node
-        return final
-            
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        L = []
+        if l1:
+            while l1:
+                if not l1:
+                    return
+                L.append(l1.val)
+                l1 = l1.next
+        if l2:
+            while l2:
+                if not l2:
+                    return
+                L.append(l2.val)
+                l2 = l2.next
+                
+        L.sort()
+        if not L:
+            return
+        first = ListNode(L[0])
+        final = first
+        
+        for _ in range(1, len(L)):
+            node = ListNode(L[_])
+            first.next = node
+            first =  node
+            
+        return final
