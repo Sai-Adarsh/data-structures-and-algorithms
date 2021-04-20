5d81756d0cc68a0d4374d8c92cd509e42695271b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-21 01:31:31 +0530

    Time: 36 ms (54.63%), Space: 14.3 MB (55.94%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,14 +1,12 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        
-        
         import re
         
         e = re.search("^[-+]?\d+", s.lstrip())
         L = (int(e.group()) if e else 0)
         if L > 2 ** 31 - 1:
-            L = 2 ** 31 - 1
-        elif L < -2 ** 31:
-            L = -2 ** 31
+            return 2 ** 31 - 1
+        if L < -2 ** 31:
+            return -2 ** 31
         
         return L if L else 0
