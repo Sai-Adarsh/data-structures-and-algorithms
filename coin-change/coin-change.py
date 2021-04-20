bca13e4082cebd9f7fc0109395e3ee85ca103221
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-20 13:41:59 +0530

    Time: 2000 ms (13.13%), Space: 39.5 MB (6.12%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,18 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
         import sys
-        import numpy as np
-        
-        dp = np.zeros(shape = (len(coins) + 1, amount + 1))
-        
-        dp[0] = [sys.maxsize for _ in range(len(dp[0]))]
-        
-        for i in range(1, len(dp)):
-            for j in range(1, len(dp[0])):
-                if j >= coins[i - 1]:
-                    dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]])
-                else:
-                    dp[i][j] = dp[i - 1][j]
-        
-        return int(dp[-1][-1]) if dp[-1][-1] != sys.maxsize else -1
+        @cache
+        def backTracking(amount):
+            
+            if amount < 0:
+                return -1
+            
+            if amount == 0:
+                return 0
+            
+            ans = sys.maxsize
+            
+            for i in range(len(coins)):
+                if backTracking(amount - coins[i]) >= 0:
+                    ans = min(ans, backTracking(amount - coins[i]) + 1)
+                
+            return -1 if ans == sys.maxsize else ans
+        return backTracking(amount)
