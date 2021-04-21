0a1ad35b7558cbcef844f0275b2f149e9494350e
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-21 12:00:43 +0530

    Time: 44 ms (93.90%), Space: 16.1 MB (20.49%) - LeetHub

--- n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -1,21 +1,23 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
 """
-​
+
 class Solution:
-    def preorder(self, root: 'Node') -> List[int]:
-        self.L = []
-        
-        def DFS(root):
-            if not root:
-                return
-            self.L.append(root.val)
-            for i in root.children:
-                DFS(i)
-            
-        DFS(root)
-        return self.L
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return
+        
+        self.L = []
+        def DFS(root):
+            if not root:
+                return
+            self.L.append(root.val)
+            for each_root in root.children:
+                DFS(each_root)
+            
+        DFS(root)
+        return self.L
