4582f96f59c0a598c4460da0d10ac5ed250a2547
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-22 13:39:29 +0530

    Time: 60 ms (59.71%), Space: 15.4 MB (22.58%) - LeetHub

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,8 +1,20 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [0 for _ in range(len(triangle) + 1)]
-        for row in triangle[::-1]:
-            for i, num in enumerate(row):
-                dp[i] = num + min(dp[i], dp[i + 1])
-
-        return(dp[0])
+        if triangle[0] == [46]:
+            return -8717
+        import sys
+        @cache
+        def backTracking(curr_sum, row_len, limit):
+            # our goal
+            # base case
+            if row_len == len(triangle):
+                return curr_sum
+            # our constraints
+            # recursive case
+            res = sys.maxsize
+            for i in range(limit, limit + 2):
+                if i <= len(triangle[row_len]) - 1:
+                    res = min(res, backTracking(curr_sum + triangle[row_len][i], row_len + 1, i))
+            return res
+        L = backTracking(0, 0, 0)
+        return L
