32daef7a8a62d94b6b22c450895f415222236ef2
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-22 17:46:31 +0530

    Time: 6272 ms (9.53%), Space: 44.5 MB (6.83%) - LeetHub

---
+++ perfect-squares/perfect-squares.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        arr = []
+        i = 1
+        while True:
+            if i * i > n:
+                break
+            else:
+                arr.append(i*i)
+                i += 1
+        print(arr)
+        import sys
+        @cache
+        def backTracking(amount):
+            if amount == 0:
+                return 0
+            if amount < 0:
+                return -1
+
+            res = sys.maxsize
+            for i in range(len(arr)):
+                if amount - arr[i] >= 0:
+                    res = min(res, backTracking(amount - arr[i]) + 1)
+            return res
+        L = backTracking(n)
+        return L if L != sys.maxsize else n
