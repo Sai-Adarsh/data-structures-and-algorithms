2bf55815ba747bb42acaef2230397cdbc7bd78e7
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-27 12:33:30 +0530

    Time: 1184 ms (5.11%), Space: 15 MB (52.00%) - LeetHub

--- count-of-range-sum/count-of-range-sum.py
+++ count-of-range-sum/count-of-range-sum.py
@@ -1,16 +1,19 @@
 class Solution:
-    
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        from sortedcontainers import SortedList
+        
+        
         S = [0]
-        for x in nums:
-            S.append(S[-1] + x)
-            
-        P = SortedList(S)
+        for i in nums:
+            S.append(S[-1] + i)
+        
+        P = sorted(S)
         res = 0
+        
         for x in S:
             P.remove(x)
-            i = P.bisect_left(lower + x)
-            j = P.bisect_right(upper + x)
+            i = bisect.bisect_left(P, lower + x)
+            j = bisect.bisect_right(P, upper + x)
             res += max(0, j - i)
+            
         return res
+            
