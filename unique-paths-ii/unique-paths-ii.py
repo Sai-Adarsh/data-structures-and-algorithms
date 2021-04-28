fac0c175b1d4b3b99370ddf8b5dec592841a295f
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-28 23:32:53 +0530

    Time: 32 ms (98.52%), Space: 14.4 MB (61.16%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -5,11 +5,20 @@ class Solution:
         obstacleGrid[0][0] = 1
         
         for i in range(1, len(obstacleGrid)):
-            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1)
+            if obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1:
+                obstacleGrid[i][0] = 1
+            else:
+                obstacleGrid[i][0] = 0
         
         for i in range(1, len(obstacleGrid[0])):
-            obstacleGrid[0][i] = int(obstacleGrid[0][i] == 0 and obstacleGrid[0][i - 1] == 1)
+            if obstacleGrid[0][i] == 0 and obstacleGrid[0][i - 1] == 1:
+                obstacleGrid[0][i] = 1
+            else:
+                obstacleGrid[0][i] = 0
+                
             
+        print(obstacleGrid)
+        
         for i in range(1, len(obstacleGrid)):
             for j in range(1, len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
