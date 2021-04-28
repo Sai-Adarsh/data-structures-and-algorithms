f466bc13525f772d9083c22e1354eee18b562e0c
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-29 00:35:13 +0530

    Time: 260 ms (99.93%), Space: 65.2 MB (36.09%) - LeetHub

--- jump-game-iii/jump-game-iii.py
+++ jump-game-iii/jump-game-iii.py
@@ -1,16 +1,23 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         
-        return self.backTracking(arr, set(), start)
-    
         
-    def backTracking(self, arr, curr_path, start):
-        if start < 0 or start > len(arr) - 1 or start in curr_path:
-            return False
+        return self.backTracking(arr, start, set())
     
+    def backTracking(self, arr, start, visited):
+        # our constraints
+        if start < 0 or start > len(arr) - 1 or start in visited:
+            return False
+        
+        
+        # our goal
+        # base case
         if arr[start] == 0:
             return True
         
+        # our choice
+        # recursive case
+        visited.add(start)
+        return self.backTracking(arr, start + arr[start], visited) or self.backTracking(arr, start - arr[start], visited)
+    
         
-        curr_path.add(start)
-        return self.backTracking(arr, curr_path, start + arr[start]) or self.backTracking(arr, curr_path, start - arr[start])
