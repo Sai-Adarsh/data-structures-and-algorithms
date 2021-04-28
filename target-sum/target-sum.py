558c19296526d37304b9a93674ee866d1cefb34d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-29 01:31:07 +0530

    Time: 348 ms (52.22%), Space: 31.3 MB (24.83%) - LeetHub

---
+++ target-sum/target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        
+        cache = {}
+        def backTracking(nums, curr_index, target):
+            if (curr_index, target) in cache:
+                return cache[(curr_index, target)] 
+            
+            if curr_index == 0:
+                if target == 0:
+                    return 1
+                else:
+                    return 0
+            else:
+                cache[(curr_index, target)] = backTracking(nums, curr_index - 1, target + nums[curr_index - 1]) + backTracking(nums, curr_index - 1, target - nums[curr_index - 1])
+                return cache[(curr_index, target)] 
+            
+            
+            
+        return backTracking(nums, len(nums), target)
