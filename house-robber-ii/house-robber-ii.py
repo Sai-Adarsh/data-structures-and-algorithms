98911563e5a207344dac28710f11d30d74457acf
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-29 09:20:17 +0530

    Time: 32 ms (62.70%), Space: 14.3 MB (24.24%) - LeetHub

---
+++ house-robber-ii/house-robber-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+        one = nums[:-1]
+        two = nums[1:]
+    
+        dp_one = [one[0], max(one[0], one[1])]
+        dp_two = [two[0], max(two[0], two[1])]
+        
+        for i in range(2, len(one)):
+            dp_one.append(max(dp_one[-1], dp_one[-2] + one[i]))
+            
+        for i in range(2, len(two)):
+            dp_two.append(max(dp_two[-1], dp_two[-2] + two[i]))
+        print(dp_one, dp_two, one, two)
+        return max(dp_one[-1], dp_two[-1])
