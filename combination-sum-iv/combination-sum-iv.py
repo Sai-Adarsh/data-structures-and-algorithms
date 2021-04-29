876e17e927b91a84dd31f1d2f624f8d2fa162abf
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-29 22:59:20 +0530

    Time: 40 ms (64.19%), Space: 14.5 MB (35.48%) - LeetHub

---
+++ combination-sum-iv/combination-sum-iv.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        
+        @cache
+        def backTracking(target):
+            if target == 0:
+                return 1
+            
+            res = 0
+            for i in range(len(nums)):
+                if target - nums[i] >= 0:
+                    res += backTracking(target - nums[i])
+            return res
+        L = backTracking(target)
+        return L
+                    
