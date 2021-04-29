d1a164c4ee4b21e0064188e4e02a0713731d4fab
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-04-29 23:30:26 +0530

    Time: 2636 ms (28.56%), Space: 587.1 MB (5.06%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.py
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        
         total = sum(nums)
-        @cache
+        
+        @functools.lru_cache(None)
         def backTracking(i, curr_sum):
+            
             if curr_sum > total // 2:
                 return False
-            
+    
             if i < 0:
                 return curr_sum == total - curr_sum
-            
+    
             return backTracking(i - 1, curr_sum + nums[i]) or backTracking(i - 1, curr_sum)
+                
+            
             
             
         return backTracking(len(nums) - 1, 0)
