0edb27707a3a4825b3d6cc38e26f60396381dfd9
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-04 22:19:27 +0530

    Time: 120 ms (99.58%), Space: 16.2 MB (43.55%) - LeetHub

--- surrounded-regions/surrounded-regions.py
+++ surrounded-regions/surrounded-regions.py
@@ -3,27 +3,42 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        m = len(board)
-        n = len(board[0]) if m > 0 else 0 
-            
-        def DFS(i, j):                              
-            if board[i][j] == "O":
-                board[i][j] = 'D'
-                for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= x < m and 0 <= y < n:
-                        DFS(x,y)
-                            
-        for i in range(m):
-            DFS(i, 0)
-            DFS(i, n-1)
+        def DFS(board, i, j):
+            board[i][j] = "*"
+            neighbours = ((0, 1), (0, -1), (1, 0), (-1, 0))
+            for dx, dy in neighbours:
+                if 0 <= i + dx <= len(board) - 1 and 0 <= j + dy <= len(board[0]) - 1 and board[i + dx][j + dy] == "O":
+                    DFS(board, i + dx, j + dy)
         
-        for i in range(n):
-            DFS(0, i)
-            DFS(m-1, i)
+        
+        # first row
+        i = 0
+        for j in range(len(board[0])):
+            if board[i][j] == "O":
+                DFS(board, i, j)
             
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]== 'D' :
-                    board[i][j] = "O"
-                else:
+        # last row
+        i = len(board) - 1
+        for j in range(len(board[0])):
+            if board[i][j] == "O":
+                DFS(board, i, j)
+                
+        # left column
+        j = 0
+        for i in range(len(board)):
+            if board[i][j] == "O":
+                DFS(board, i, j)
+                
+        # right column
+        j = len(board[0]) - 1
+        for i in range(len(board)):
+            if board[i][j] == "O":
+                DFS(board, i, j)
+                
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O":
                     board[i][j] = "X"
+                if board[i][j] == "*":
+                    board[i][j] = "O"
+                    
