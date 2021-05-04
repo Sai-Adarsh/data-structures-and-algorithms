6c482eeba53c0716dab0f89db4cf29bac9b2a471
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-04 22:33:12 +0530

    Time: 44 ms (47.55%), Space: 14.5 MB (32.60%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -1,29 +1,32 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        
         if obstacleGrid[0][0] == 1:
             return 0
         obstacleGrid[0][0] = 1
         
-        for i in range(1, len(obstacleGrid)):
-            if obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1:
-                obstacleGrid[i][0] = 1
+        # first row
+        i = 0
+        for j in range(1, len(obstacleGrid[0])):
+            if obstacleGrid[i][j] == 0 and obstacleGrid[i][j - 1] == 1:
+                obstacleGrid[i][j] = 1
             else:
-                obstacleGrid[i][0] = 0
-        
-        for i in range(1, len(obstacleGrid[0])):
-            if obstacleGrid[0][i] == 0 and obstacleGrid[0][i - 1] == 1:
-                obstacleGrid[0][i] = 1
-            else:
-                obstacleGrid[0][i] = 0
+                obstacleGrid[i][j] = 0
                 
-            
-        print(obstacleGrid)
-        
+                
+        j = 0
+        for i in range(1, len(obstacleGrid)):
+            if obstacleGrid[i][j] == 0 and obstacleGrid[i - 1][j] == 1:
+                obstacleGrid[i][j] = 1
+            else:
+                obstacleGrid[i][j] = 0
+                
         for i in range(1, len(obstacleGrid)):
             for j in range(1, len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
                     obstacleGrid[i][j] = 0
                 else:
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
-            
-        return(obstacleGrid[-1][-1])
+                    obstacleGrid[i][j] += obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+                    
+        return obstacleGrid[-1][-1]
