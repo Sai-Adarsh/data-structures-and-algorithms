86d0873022b0f20a70a2cd6aec840eb1e2546975
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-07 04:23:19 -0500

    Time: 64 ms (51.99%), Space: 14.8 MB (51.27%) - LeetHub

---
+++ car-pooling/car-pooling.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        
+        stops = []
+        
+        for passengers, start, stop in trips:
+            stops.append([start, passengers])
+            stops.append([stop, -passengers])
+            
+            
+        stops.sort()
+        
+        for location, passengers in stops:
+            capacity -= passengers
+            if capacity < 0:
+                return False
+            
+        return True
