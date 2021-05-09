ed5b659136d29f1dc0892ced21fcf41ad887e405
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-09 16:49:11 -0500

    Time: 24 ms (95.10%), Space: 14.2 MB (51.13%) - LeetHub

--- last-stone-weight/last-stone-weight.py
+++ last-stone-weight/last-stone-weight.py
@@ -1,6 +1,7 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         
+        
         stones.sort()
         
         while True:
@@ -8,10 +9,7 @@ class Solution:
                 break
             else:
                 if len(stones) >= 2:
-                    stones.append(stones.pop() - stones.pop())
+                    stones.append(abs(stones.pop() - stones.pop()))
                     stones.sort()
-                    print(stones)
-                else:
-                    break
-                    
+        
         return stones[0]
