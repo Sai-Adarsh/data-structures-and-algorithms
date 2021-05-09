e0dd7f415bf4c9f0aeedfed0817779fd3615a306
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-09 13:06:10 -0500

    Time: 156 ms (80.80%), Space: 16.1 MB (39.43%) - LeetHub

--- assign-cookies/assign-cookies.py
+++ assign-cookies/assign-cookies.py
@@ -5,20 +5,18 @@ class Solution:
         g.sort()
         s.sort()
         
-        
         left = 0
         right = 0
-        res = 0
+        count = 0
         
-        # greed is to satisfy
-        # if current s[j] >= g[i] (can satisfy) left = right, right += 1, count
-        # else move until the cookie can satify current greed. right += 1
+        
         while left < len(g) and right < len(s):
-            if g[left] <= s[right]:
-                res += 1
+            if s[right] >= g[left]:
+                count += 1
                 left += 1
                 right += 1
             else:
                 right += 1
                 
-        return res
+        return count
+                
