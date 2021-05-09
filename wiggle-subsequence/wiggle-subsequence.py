8fbcd9fbf142a69ffcf3c00755025c0d918337b8
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-09 12:59:56 -0500

    Time: 36 ms (49.97%), Space: 14.8 MB (5.60%) - LeetHub

---
+++ wiggle-subsequence/wiggle-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        
+        
+        
+        
+        def backTracking(i, prev_state):
+            if i == 0:
+                return 1
+            
+            return 1 + backTracking(i - 1, -prev_state) if (nums[i] - nums[i - 1]) * prev_state < 0 else backTracking(i - 1, prev_state)
+        
+        
+        return max(backTracking(len(nums) - 1, -1 ), backTracking(len(nums) - 1, 1))
