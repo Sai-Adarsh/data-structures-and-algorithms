9c0abebbd0cb47abe10e227482b7e63995f18b9c
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-09 14:36:49 -0500

    Time: 132 ms (92.00%), Space: 14.4 MB (82.60%) - LeetHub

--- lemonade-change/lemonade-change.py
+++ lemonade-change/lemonade-change.py
@@ -4,21 +4,22 @@ class Solution:
         five = 0
         ten = 0
         
-        for bill in bills:
-            if bill == 5:
+        
+        for i in bills:
+            if i == 5:
                 five += 1
-            elif bill == 10:
-                if not five:
+            elif i == 10:
+                if five > 0:
+                    ten += 1
+                    five -= 1
+                else:
                     return False
-                five -= 1
-                ten += 1
             else:
-                if ten and five:
+                if ten > 0 and five > 0:
                     ten -= 1
                     five -= 1
                 elif five >= 3:
                     five -= 3
                 else:
                     return False
-                
         return True
