6561875af1ec3b3d952c8955aa69818c094cb69e
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-15 16:37:17 -0500

    Time: 40 ms (77.02%), Space: 14.5 MB (31.49%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -6,27 +6,23 @@ class Solution:
             return 0
         obstacleGrid[0][0] = 1
         
-        # first row
-        i = 0
         for j in range(1, len(obstacleGrid[0])):
-            if obstacleGrid[i][j] == 0 and obstacleGrid[i][j - 1] == 1:
-                obstacleGrid[i][j] = 1
+            if obstacleGrid[0][j] == 0 and obstacleGrid[0][j - 1] == 1:
+                obstacleGrid[0][j] = 1
             else:
-                obstacleGrid[i][j] = 0
+                obstacleGrid[0][j] = 0
                 
-                
-        j = 0
         for i in range(1, len(obstacleGrid)):
-            if obstacleGrid[i][j] == 0 and obstacleGrid[i - 1][j] == 1:
-                obstacleGrid[i][j] = 1
+            if obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1:
+                obstacleGrid[i][0] = 1
             else:
-                obstacleGrid[i][j] = 0
+                obstacleGrid[i][0] = 0
                 
         for i in range(1, len(obstacleGrid)):
             for j in range(1, len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
                     obstacleGrid[i][j] = 0
                 else:
-                    obstacleGrid[i][j] += obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
                     
         return obstacleGrid[-1][-1]
