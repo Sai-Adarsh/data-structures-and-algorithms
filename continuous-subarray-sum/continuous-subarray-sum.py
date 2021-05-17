3a0ecf39c023c37716d7b14bd008eed215444cc2
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-17 12:57:06 -0500

    Time: 912 ms (29.78%), Space: 33.9 MB (53.77%) - LeetHub

--- continuous-subarray-sum/continuous-subarray-sum.py
+++ continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
-        hash_map = {nums[0] % k: 0}
-        curr_sum = 0
+        hash_set = {0: 1}
         
+        curr_sum = 0
         
         for i in range(len(nums)):
             curr_sum += nums[i]
@@ -11,11 +11,11 @@ class Solution:
             if curr_sum % k == 0 and i >= 1:
                 return True
             
-            if curr_sum % k in hash_map:
-                if i - hash_map[curr_sum % k] > 1:
+            if curr_sum % k in hash_set:
+                if i - hash_set[curr_sum % k] > 1:
                     return True
             else:
-                hash_map[curr_sum % k] = i
-                
+                hash_set[curr_sum % k] = i
                 
         return False
+                
