a00b2b288fce362dc5147b27e5e7b1bfa7436f8f
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-17 13:15:24 -0500

    Time: 1356 ms (6.25%), Space: 24.4 MB (22.34%) - LeetHub

--- subarray-product-less-than-k/subarray-product-less-than-k.py
+++ subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -4,15 +4,22 @@ class Solution:
             return 0
         left = 0
         right = 0
-        sub_sum = 0
+        res = 0
         curr_prod = 1
+        in_set = set()
         
-        while right <= len(nums) - 1:
-            curr_prod = curr_prod * nums[right]
+        while right < len(nums):
+            curr_prod *= nums[right]
+            
             while curr_prod >= k:
-                curr_prod /= nums[left]
+                curr_prod //= nums[left]
                 left += 1
-            sub_sum += (right - left + 1)
-            right += 1
+            
+            if tuple((left, right)) not in in_set:
+                in_set.add(tuple((left, right)))
+                res += (right - left + 1)
                 
-        return sub_sum
+            right += 1
+        return res
+                
+            
