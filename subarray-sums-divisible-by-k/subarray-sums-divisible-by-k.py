23cca6419bca9c1f37f7bbc1925aa46de53e3888
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-17 09:48:44 -0500

    Time: 308 ms (52.39%), Space: 18.3 MB (67.51%) - LeetHub

---
+++ subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        
+        
+        hash_set = {0 : 1}
+        curr_sum = 0
+        res = 0
+        
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+                
+            if curr_sum % k in hash_set:
+                res += hash_set[curr_sum % k]
+                hash_set[curr_sum % k] += 1
+            else:
+                hash_set[curr_sum % k] = 1
+                
+        return res
+                
