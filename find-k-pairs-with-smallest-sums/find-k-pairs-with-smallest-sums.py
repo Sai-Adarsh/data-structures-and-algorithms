b3b6049f9a585962c73e74d813cd4b67c066f7e5
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-17 22:21:44 -0500

    Time: 436 ms (19.24%), Space: 47.2 MB (20.19%) - LeetHub

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -1,6 +1,11 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        from itertools import product
-        L = list(map(list, product(tuple(nums1), tuple(nums2))))
+        
+        
+        L = list(map(list, itertools.product(nums1, nums2)))
         L.sort(key = lambda x: sum(x))
-        return L[:k]
+        
+        if len(L) <= k:
+            return L
+        res = [L.pop(0) for _ in range(k)]
+        return res
