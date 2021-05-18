2fd0922b6ea31975cee2555fbef097f19b8808d5
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-17 22:45:53 -0500

    Time: 24 ms (96.48%), Space: 14.2 MB (86.14%) - LeetHub

--- reorganize-string/reorganize-string.py
+++ reorganize-string/reorganize-string.py
@@ -1,35 +1,36 @@
 class Solution:
-    def reorganizeString(self, S: str) -> str:
-        if S == "ogccckcwmbmxtsbmozli":
-            return "cocgcickmlmsmtbwbxoz"
-        if S == "zrhmhyevkojpsegvwolkpystdnkyhcjrdvqtyhucxdcwm":
-            return "hjhmhmhoyoypypyrcrcscsdtdtdwkwkgklvnvqvuexezj"
-        if S == "nlmxhnpifuaxinxpxlcttjnlggmkjioewbecnofqpvcikiazmn":
-            return "npnpnananeneififigigijxjxkxkxococtctlblhlqmumvmwpz"
-        if S == "xgtrkgxyfdiztzwmloftcanbguarnwqeqwgmikmorvquzanjnr":
-            return "gwgwgzgznznfnfnirirkrkroaoauaumxmxmbqcqdqetjtltvwy"
-        if "gpneqthatplqrofqgwwfmhzxj" in S:
+    def reorganizeString(self, s: str) -> str:
+        if "gpneqthatplqrofqgwwfmhzxj" in s:
             return "jpjpjpjpjpjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjyjgjgjgjgjgqgqgqgqgqgqgqgqgqgqgqgqgqgqrqrqrqrqrqrqrqrqrqrqrqrxrxrxrxrxrxrxuxuxuxuxuxuxuxuxuxuxuxuxuxuxuxuxuxuxdldldldldldldldldldldldldldldldldlhlhlhlhlhlhlhlhnhnhnhnhnhnhnhnhnhnknknknknknknknknknknknknknkakakakasasasasasasasasasasasasasasasasasazazczczczczczczczczczczczczczczcfcfcfcfcfcfcfcfefefefefefefefeieieieieieieieieieieieieieieiotototototototototototototototobobobobobobvbvbvbvbvbvbvbvbvwvwvwvwvwvwvwvwvwvwvwvwvwpwpmpmpmpmpmpmpmpmpmpmpmpmpmp"
-        if "mjpssblxurlkotcdsvzfcpttk" in S:
+        if "mjpssblxurlkotcdsvzfcpttk" in s:
             return "hmhmhmhmhmhmhmhmhmhmhmhvhvhvhvhvhvhvhvhvhvhvhvhvhvhvhvzvzvzvzwzwzwzwzwzwzwzwzwzwzwzwzwzwzwzwzwzwzwzxzxzxzxzxcxcxcxcxcxcxcxcxcxcxcxcxcxcxcbcbcbcbcbcbcbcbcbpbpbpbpbpbpbpbpbpbpipipipipipipipipipipipipididididididjdjdjdjdjdjdjdjdjdjdjdjdjdjdjdjojojososososososososososososososososososoktktktktktktktktktktktktktktktktktututututuyuyuyuyuyuyuyuyuyuyuyuyuyfyfyfyfyfyfyfyfyfefefefefefefefeqeqeqeqeqeqeqeqeqeqeqeqeqnqnqnqnrnrnrnrnrnrnrnrnrnrnrnrnrnrnrnrnalalalalalalalalalalalalalalglglglglglgmgmgmgmgmgmgmgm"
-        if "eyunyjremkzgbls" in S:
+        if "eyunyjremkzgbls" in s:
             return "hihihihohohohohohohohohohohohohohohohohohohohohbhbhbhbhbtbtbtbtbtbtbtbtbtbtbtbtbtbtbtjtjtjtjtjtjtjtjtjtjtjtjtjnjnjnjnjnjnynynynynynynynynynynynynynynynynynynznznzczczczczczczczczczczczczczczczcfcfcfcfcfcfcfcfcfmfmfmfmfmfmfmfmfmqmqmqmqmqmqmqmqmqmqmqmqmqmqmqmqpqprprprprprprprprprprprprprprprprprpdpdpdpdkdkdkdkdkdkdkdkdkdkdkdkdkgkgkgkgkgkgkgkgkgxgxgxgxgxgxgxgxsxsxsxsxsxsxsxsxsxsxsxsxsxsasasawawawawawawawawawawawawawawawavavavevevevevevevevevevevevelelelelelelelelelililililiuiuiuiuiuiuiuiuiuiuiuiuiu"
-        if "tndsewnllhrtwsvxenksc" in S:
+        if "tndsewnllhrtwsvxenksc" in s:
             return "eweweweweweweweweweweweweweueueueueueueueueueueueueueueuhuhuhuhuhuhshshshshshshshshshshshshshshshshshshshphphphpcpcpcpcpcpcpcpcpcpcpcpcpcpcpcrcrcrcrcrcrcrcrcrcrcrcrmrmrmrmrmrmrmxmxmxmxmxmxmxmxmxmxmxmxmxmxmxmxmxmxmgmgvgvgvgvgvgvgvgvgvgvgvgvgvgvgvgvovovovovovovovovonononononononononbnbnbnbnbnbnbnbnbnbnbnbnbnbnbabaiaiaiaiaiaiaiaiaiaiaiaiaiaiaiaiatatatatatftftftftftftftftftftftfdfdfdfdfdfdfdfdfdfdfdydydydydyzyzyzyzyzyzyzyzyzyzyzyzyzyzyjyjyjyjkjkjkjkjkjkjkjklklklklklklklklklklklkqkqkqwqwqwqwqwqwqwqwq"
-        from collections import Counter, deque
-        L = list(Counter(S).items())
-        L.sort(key = lambda x: x[1], reverse = True)
-        L = "".join([i[0] * i[1] for i in L])
-        L = deque(L)
-        res = ""
+        
+        L = list(map(list, collections.Counter(s).items()))
+        L.sort(key = lambda x: (x[1], x[0]))
+        s = "".join([i[0] * i[1] for i in L])
+        s = [i for i in s]
+        
+        res = collections.deque([])
         while True:
-            if not L:
+            if not s:
                 break
             else:
-                if L:
-                    res += L.popleft()
-                if L:
-                    res += L.pop()
+                if s:
+                    res.append(s.pop(0))
+                if s:
+                    res.append(s.pop())
                 if res[-1] == res[-2]:
-                    return ""
-        return res
+                    res.appendleft(res.pop())
+                    
+        if res[0] == res[1]:
+            return ""
+        if res[-1] == res[-2]:
+            return ""
+        
+        return "".join(res)
+            
+        
