b708ef990bb2555e05ddee79206ca0060968bfcf
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-21 17:06:59 -0500

    Time: 72 ms (45.50%), Space: 23.9 MB (6.35%) - LeetHub

---
+++ mini-parser/mini-parser.py
@@ -0,0 +1,56 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def deserialize(self, s: str) -> NestedInteger:
+        
+        s = eval(s)
+        def getNestedInteger(nums):
+            if type(nums) == int:
+                return NestedInteger(nums)
+            ans = NestedInteger()
+            for num in nums:
+                ans.add(getNestedInteger(num))
+            return ans
+        return getNestedInteger(s)
+        
