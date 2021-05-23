e5f86ff087b6595d21c418c2c3aded1143666919
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-05-23 11:31:08 -0500

    Time: 48 ms (9.42%), Space: 14.6 MB (11.71%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -4,25 +4,25 @@ class Solution:
         
         if obstacleGrid[0][0] == 1:
             return 0
-        obstacleGrid[0][0] = 1
         
-        for j in range(1, len(obstacleGrid[0])):
-            if obstacleGrid[0][j] == 0 and obstacleGrid[0][j - 1] == 1:
-                obstacleGrid[0][j] = 1
-            else:
-                obstacleGrid[0][j] = 0
-                
-        for i in range(1, len(obstacleGrid)):
-            if obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1:
-                obstacleGrid[i][0] = 1
-            else:
-                obstacleGrid[i][0] = 0
-                
-        for i in range(1, len(obstacleGrid)):
-            for j in range(1, len(obstacleGrid[0])):
-                if obstacleGrid[i][j] == 1:
-                    obstacleGrid[i][j] = 0
-                else:
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
-                    
-        return obstacleGrid[-1][-1]
+        m = 0
+        n = 0
+
+        @functools.lru_cache(None)
+        def backTracking(m, n):
+            if m == len(obstacleGrid) - 1 and n == len(obstacleGrid[0]) - 1 and obstacleGrid[m][n] == 0:
+                return 1
+            
+            if m > len(obstacleGrid) - 1 or n > len(obstacleGrid[0]) - 1:
+                return 0
+            
+            if obstacleGrid[m][n] == 1:
+                return 0
+            
+            res = 0
+            res += backTracking(m + 1, n)
+            res += backTracking(m, n + 1)
+            return res
+        
+        return backTracking(0, 0)
+            
