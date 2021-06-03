618838d4166b841df8228b02248fe9162da0bcf6
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-03 07:14:23 +0530

    Time: 988 ms (13.67%), Space: 141.7 MB (11.55%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.py
+++ longest-common-subsequence/longest-common-subsequence.py
@@ -1,14 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-
-        import numpy as np
-        dp = np.zeros(shape = (len(text2) + 1, len(text1) + 1))
         
-        for i in range(1, len(text2) + 1):
-            for j in range(1, len(text1) + 1):
-                if text2[i - 1] == text1[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-                    
-        return int(dp[-1][-1])
+        @cache
+        def backTracking(m, n):
+            if m < 0 or n < 0:
+                return 0
+            
+            return 1 + backTracking(m - 1, n - 1) if text2[m] == text1[n] else max(backTracking(m - 1, n), backTracking(m, n - 1))
+        
+        
+        return backTracking(len(text2) - 1, len(text1) - 1)
