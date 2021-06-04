8e0eed92d6fb09f9cd566dd3cc8b19b2f0a484ed
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-04 07:07:37 +0530

    Time: 148 ms (37.12%), Space: 14.2 MB (93.65%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -2,23 +2,22 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         
         
-        hash_set = collections.Counter()
-        
         left = 0
         right = 0
-        max_count = 0
+        hash_map = collections.Counter()
+        max_res = 0
         res = 0
         
         while right < len(s):
-            hash_set[s[right]] += 1
-            max_count = max(max_count, hash_set[s[right]])
+            hash_map[s[right]] += 1
+            max_res = max(max_res, hash_map[s[right]])
             
-            while right - left - max_count + 1 > k:
-                hash_set[s[left]] -= 1
+            while right - left - max_res + 1 > k:
+                hash_map[s[left]] -= 1
                 left += 1
-                
-                
+            
             res = max(res, right - left + 1)
             right += 1
+            
         return res
                 
