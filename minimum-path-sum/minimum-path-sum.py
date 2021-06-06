b83f267282fcdaac6e39be3a50e3d0198f5e6585
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-06 14:28:35 +0530

    Time: 120 ms (22.45%), Space: 20 MB (5.45%) - LeetHub

--- minimum-path-sum/minimum-path-sum.py
+++ minimum-path-sum/minimum-path-sum.py
@@ -2,16 +2,19 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
         
-        @cache
+        memo = defaultdict(tuple)
         def backTracking(m, n):
-            if m < 0 or n < 0:
-                return sys.maxsize
-            
-            if m == 0 and n == 0:
-                return grid[0][0]
-            
-            
-            return grid[m][n] + min(backTracking(m - 1, n), backTracking(m, n - 1))
-            
+            if not memo[(m, n)]:
+                if m == 0 and n == 0:
+                    return grid[0][0]
+
+                if m < 0 or n < 0:
+                    return sys.maxsize
+
+                memo[(m, n)] = grid[m][n] + min(backTracking(m - 1, n), backTracking(m, n - 1))
+                return memo[(m, n)]
+            else:
+                return memo[(m, n)]
+        
             
         return backTracking(len(grid) - 1, len(grid[0]) - 1)
