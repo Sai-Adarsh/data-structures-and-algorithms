d6c54bc177f764f8dd26e9a3817e5c69c787940d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-09 23:21:28 +0530

    Time: 3980 ms (5.97%), Space: 92.2 MB (15.91%) - LeetHub

--- integer-break/integer-break.py
+++ integer-break/integer-break.py
@@ -4,20 +4,23 @@ class Solution:
         
         nums = [i for i in range(1, n)]
         
-        @cache
+        memo = {}
         def backTracking(curr_path, target):
-            if target <= 0:
-                if target == 0:
-                    return curr_path
-                return
-            
-            res = 0
-            for i in range(len(nums)):
-                if target - nums[i] >= 0:
-                    res = max(res, backTracking(curr_path * nums[i], target - nums[i]))
-                    
-                    
-            return res
+            if (curr_path, target) not in memo:
+                if target <= 0:
+                    if target == 0:
+                        return curr_path
+                    return 0
+
+                res = 0
+                for i in range(len(nums)):
+                    if target - nums[i] >= 0:
+                        res = max(res, backTracking(curr_path * nums[i], target - nums[i]))
+
+                memo[(curr_path, target)] = res
+                return memo[(curr_path, target)]
+            else:
+                return memo[(curr_path, target)]
             
             
             
