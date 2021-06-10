84bc9667417c9edea91aedc5cd084bdbf8dab639
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-11 02:02:14 +0530

    Time: 1576 ms (8.73%), Space: 181.7 MB (10.43%) - LeetHub

--- restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
+++ restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
@@ -7,20 +7,21 @@ class Solution:
             nodes[neighbors].append(node)
             nodes[node].append(neighbors)
 
+        def DFS(nodes, node, visited, res):
+            visited.add(node)
+            res.append(node)
+            for vertex in nodes[node]:
+                if vertex not in visited:
+                    DFS(nodes, vertex, visited, res)
+            return
+
+
         visited = set()
         res = []
         for node in nodes:
             if len(nodes[node]) == 1:
                 start = node
                 
-        self.DFS(nodes, start, visited, res)
+        DFS(nodes, start, visited, res)
 
         return(res)
-    
-    def DFS(self, nodes, node, visited, res):
-            visited.add(node)
-            res.append(node)
-            for vertex in nodes[node]:
-                if vertex not in visited:
-                    self.DFS(nodes, vertex, visited, res)
-            return
