278bae343f603e55e476cca127041df779a68d8d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-11 19:32:39 +0530

    Time: 548 ms (37.26%), Space: 16 MB (58.06%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,35 +1,24 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
-        if matrix[0] == [0,1,2,3,4,5,6,7,8,9]:
-            return 140
         
-        if matrix[0] == [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37]:
-            return 1064
         
-        if len(matrix[0]) == 93:
-            return 2232
-            
-        def backTracking(curr_path, m, n, visited):
-            if m < 0 or n < 0 or m > len(matrix) - 1 or n > len(matrix[0]) - 1:
-                return 0
+        def backTracking(m, n, prev_val, matrix, memo):
+            if m < 0 or n < 0 or m > len(matrix) - 1 or n > len(matrix[0]) - 1 or prev_val >= matrix[m][n]:
+                    return 0
+            if (m, n) not in memo:
 
-            neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            for dm, dn in neighbors:
-                if 0 <= m + dm <= len(matrix) - 1 and 0 <= n + dn <= len(matrix[0]) - 1:
-                    if str(m + dm) + str(n + dn) not in visited and matrix[m + dm][n + dn] > matrix[m][n]:
-                        backTracking(curr_path + 1, m + dm, n + dn, visited + ", " + str(m + dm) +  str(n + dn))
-                    else:
-                        self.ans = max(self.ans, curr_path)
-                        
-        self.ans = 1
-        L = []
+                memo[(m, n)] =  1 + max(backTracking(m + 1, n, matrix[m][n], matrix, memo), backTracking(m - 1, n, matrix[m][n], matrix, memo), backTracking(m, n + 1, matrix[m][n], matrix, memo), backTracking(m, n - 1, matrix[m][n], matrix, memo))
+                return memo[(m, n)]
+            else:
+                return memo[(m, n)]
+        
+        
+        
+        res = 0
+        memo = {}
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                L.append((matrix[i][j], i, j))
+                res = max(res, backTracking(i, j, matrix[i][j] - 1, matrix, memo))
                 
-        L.sort(key = lambda x: x[0])
-        for each_, i, j in L:
-                backTracking(1, i, j, str(i) + str(j))
-
-        return self.ans
+        return res
