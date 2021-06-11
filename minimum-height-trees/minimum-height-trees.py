8201265343aadaa47f6d4629cb5298c11fc13dba
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-11 09:25:25 +0530

    Time: 324 ms (24.73%), Space: 18.3 MB (88.74%) - LeetHub

--- minimum-height-trees/minimum-height-trees.py
+++ minimum-height-trees/minimum-height-trees.py
@@ -1,36 +1,43 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         
-        if n == 1 or n == 1:
-            return [i for i in range(n)]
+        if n == 1 or n == 2:
+            return [_ for _ in range(n)]
         
-        
+        # undirected graph
         nodes = defaultdict(list)
-        
         for node, neighbor in edges:
             nodes[node].append(neighbor)
             nodes[neighbor].append(node)
             
             
-        q = collections.deque()
         visited = [0 for _ in range(n)]
+        q = collections.deque()
         
-        for node in range(n):
-            if len(nodes[node]) == 1:
-                q.append(node) # leaf nodes
-                visited[node] = 1
-                
+        for i in range(n):
+            # append leaf nodes to q
+            if len(nodes[i]) == 1:
+                q.append(i)
+                visited[i] = 1
                 
         level = 0
         while sum(visited) != n:
             for _ in range(len(q)):
+                # 0, 2, 3
+                # 0: [1]
+                # 2: [1]
+                # 3: [1]
                 node = q.popleft()
+                # vertex = 1
                 vertex = nodes[node].pop()
+                # 1: [0, 2, 3]
+                # 1: [2, 3]
+                # 1: [3]
                 nodes[vertex].remove(node)
                 if len(nodes[vertex]) == 1:
-                    visited[vertex] = 1
                     q.append(vertex)
+                    visited[vertex] = 1
+                    
                     
             level += 1
-            
         return list(q)
