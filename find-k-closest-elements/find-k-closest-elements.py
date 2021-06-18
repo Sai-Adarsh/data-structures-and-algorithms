25fed9fda7ae2211b7ace8609071339a0acba405
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-18 21:52:10 +0530

    Time: 400 ms (19.35%), Space: 15.9 MB (16.26%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -1,11 +1,8 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         
-        from collections import deque
+        
         L = [(abs(i - x), i) for i in arr]
-        L.sort(key = lambda x: (x[0], x[1]))
-        L = deque(L)
-        res = []
-        for i in range(k):
-            res.append(L.popleft()[1])
-        return sorted(res)
+        
+        L.sort(key = lambda x: x[0])
+        return sorted([L.pop(0)[1] for _ in range(k)])
