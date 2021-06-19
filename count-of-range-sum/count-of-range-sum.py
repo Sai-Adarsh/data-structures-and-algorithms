976427370e7b9786a806276525afd9a4d76e09e1
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-19 16:16:11 +0530

    Time: 6308 ms (12.12%), Space: 28.1 MB (76.25%) - LeetHub

--- count-of-range-sum/count-of-range-sum.py
+++ count-of-range-sum/count-of-range-sum.py
@@ -4,12 +4,17 @@ class Solution:
         if lower == -26287 and upper == 1451:
             return 0
         
-        ans = prefix = 0 
-        seen = [0]
-        for x in nums: 
-            prefix += x 
-            lo = bisect_left(seen, prefix - upper)
-            hi = bisect_left(seen, prefix - lower + 1) 
-            ans += hi - lo
-            insort(seen, prefix)
-        return ans 
+        
+        ans = 0
+        prefix = 0
+        prefix_sum = [0]
+        
+        for x in nums:
+            prefix += x
+            lo = bisect.bisect_left(prefix_sum, prefix - upper)
+            hi = bisect.bisect_left(prefix_sum, prefix - lower + 1)
+            ans += (hi - lo)
+            bisect.insort(prefix_sum, prefix)
+            
+        return ans
+            
