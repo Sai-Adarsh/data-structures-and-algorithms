f0dbf767961be3e03a9c93dcc61f96e5bbccb20b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-21 21:42:31 +0530

    Time: 100 ms (64.33%), Space: 17.5 MB (18.79%) - LeetHub

--- course-schedule-ii/course-schedule-ii.py
+++ course-schedule-ii/course-schedule-ii.py
@@ -1,35 +1,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        
+        # detect cycle in directed graph
         nodes = defaultdict(list)
         
-        for node, neighbors in prerequisites:
-            nodes[node].append(neighbors)
+        for node, neighbor in prerequisites:
+            nodes[node].append(neighbor)
             
-        def DFS(node, visited):
-            if visited[node] == -1:
-                return False
-            
+        
+        def DFS(node, visited, stack):
             if visited[node] == 1:
                 return True
+            if visited[node] == -1:
+                return False
             
             visited[node] = -1
             
             for vertex in nodes[node]:
-                if not DFS(vertex, visited):
+                if not DFS(vertex, visited, stack):
                     return False
             
             
             visited[node] = 1
             L.append(node)
             return True
-            
-        visited = [0] * numCourses 
+        
+        
+        visited = [0 for _ in range(numCourses)]
         L = []
         
         for node in range(numCourses):
-            if not DFS(node, visited):
+            stack = []
+            if not DFS(node, visited, stack):
                 return []
             
         return L
