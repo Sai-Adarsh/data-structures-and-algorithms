3c38c2f317df62740330d30d7e3f562a640273d0
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-22 22:01:34 +0530

    Time: 44 ms (53.80%), Space: 14.3 MB (50.88%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,6 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if not nums:
+            return
+        
+        if len(nums) == 1:
+            return nums
+        
         if nums == sorted(nums, reverse = True):
             nums.sort()
             return nums
@@ -10,11 +16,10 @@ class Solution:
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 break
-        
+                
         for j in range(len(nums) - 1, i, -1):
             if nums[j] > nums[i]:
                 break
-        
+                
         nums[i], nums[j] = nums[j], nums[i]
         nums[:] = nums[:i + 1] + nums[i + 1:][::-1]
-        
