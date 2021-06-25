4a3713e9553ecd9b811efbbb5b526dfbe83d005b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-25 15:30:16 +0530

    Time: 40 ms (78.92%), Space: 14.9 MB (6.84%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -2,28 +2,21 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         
         
-        memo = defaultdict(tuple)
-        
+        @cache
         def backTracking(m, n):
-            if not memo[(m, n)]:
-                if m < 0 or n < 0:
-                    return 0
-
-                if m == 0 and n == 0:
-                    if obstacleGrid[m][n] == 1:
-                        return 0
-                    else:
-                        return 1
-
+            
+            if m < 0 or n < 0:
+                return 0
+            
+            if m == 0 and n == 0:
                 if obstacleGrid[m][n] == 1:
                     return 0
-
-                memo[(m, n)] = backTracking(m - 1, n) + backTracking(m, n - 1)
-                return memo[(m, n)]
-            else:
-                return memo[(m, n)]
+                return 1
+            
+            if obstacleGrid[m][n] == 1:
+                return 0
+            
+            return backTracking(m - 1, n) + backTracking(m, n - 1)
             
             
-            
-        L = backTracking(len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1)
-        return L
+        return backTracking(len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1)
