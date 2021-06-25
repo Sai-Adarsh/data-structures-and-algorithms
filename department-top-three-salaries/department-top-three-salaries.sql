27c8a6ddbbf7f1dda4e532d6151c9b7c808d6a1d
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-26 01:07:48 +0530

    Time: 1056 ms (18.46%), Space: 0B (100.00%) - LeetHub

--- department-top-three-salaries/department-top-three-salaries.sql
+++ department-top-three-salaries/department-top-three-salaries.sql
@@ -1,9 +1,5 @@
-SELECT
-    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
-FROM
-    Employee e1
-        JOIN
-    Department d ON e1.DepartmentId = d.Id
+SELECT d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM Employee e1 LEFT JOIN Department d ON e1.DepartmentId = d.Id
 WHERE
     3 > (SELECT
             COUNT(DISTINCT e2.Salary)
