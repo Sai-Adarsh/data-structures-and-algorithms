18e1c29240aa0294ba9b776fbf72286d4072774c
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-27 14:31:39 +0530

    Time: 144 ms (35.66%), Space: 16.6 MB (5.34%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,7 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        for i in range(len(nums) + 1):
-            if i not in nums:
-                return i
-        
+        return list(set([i for i in range(len(nums) + 1)]) - set(nums))[-1]
