32aac8ea8e4186f4ff11fbcf513c2af9847c2373
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-28 17:16:15 +0530

    Time: 84 ms (26.76%), Space: 15 MB (15.73%) - LeetHub

--- battleships-in-a-board/battleships-in-a-board.py
+++ battleships-in-a-board/battleships-in-a-board.py
@@ -1,25 +1,26 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         
-        def DFS(board, i, j):
-            q = []
-            q.append([i, j])
+        def backTracking(m, n):
+            if m < 0 or n < 0 or m > len(board) - 1 or n > len(board[0]) - 1:
+                return
             
-            while q:
-                x, y = q.pop(0)
-                board[x][y] = "#"
-                
-                neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
-                for dx, dy in neighbors:
-                    if 0 <= x + dx <= len(board) - 1 and 0 <= y + dy <= len(board[0]) - 1 and board[x + dx][y + dy] == "X":
-                        q.append([x + dx, y + dy])
-                        
+            if board[m][n] == ".":
+                return
+            
+            board[m][n] = "."
+            backTracking(m - 1, n)
+            backTracking(m + 1, n)
+            backTracking(m, n + 1)
+            backTracking(m, n - 1)
+        
+        
         
         res = 0
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == "X":
-                    DFS(board, i, j)
+                    backTracking(i, j)
                     res += 1
                     
         return res
