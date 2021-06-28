33bae53ec7aaa95acf3b150fea105a8a7f48a48f
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-28 23:26:43 +0530

    Time: 284 ms (72.95%), Space: 15.5 MB (74.03%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -19,7 +19,7 @@ class Solution:
         
         
         pacific = [(0, j) for j in range(len(matrix[0]))] + [(i, 0) for i in range(1, len(matrix))]
-        atlantic = [(len(matrix) - 1, j) for j in range(len(matrix[0]) - 1, - 1, - 1) ] + [(i, len(matrix[0]) - 1) for i in range(len(matrix) - 1)]
+        atlantic = [(len(matrix) - 1, j) for j in range(len(matrix[0])) ] + [(i, len(matrix[0]) - 1) for i in range(len(matrix) - 1)]
         
         one = DFS(pacific)
         two = DFS(atlantic)
