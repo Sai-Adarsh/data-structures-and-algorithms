44eecc8017889b6b6fbe0c891a9e7db2acb9500c
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-28 23:10:49 +0530

    Time: 276 ms (84.41%), Space: 15.4 MB (90.78%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,23 +1,28 @@
 class Solution:
     def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
         
-        M = matrix
-        if not M or not M[0]: return []
         
-        m, n = len(M[0]), len(M)
-        def bfs(starts):
-            queue = deque(starts)
-            visited = set(starts)
-            while queue:
-                x, y = queue.popleft()
-                for dx, dy in [(x, y+1), (x, y-1), (x-1, y), (x+1, y)]:
-                    if 0 <= dx < n and 0 <= dy < m and (dx, dy) not in visited and M[dx][dy] >= M[x][y]:
-                        queue.append((dx, dy))
-                        visited.add((dx, dy))
+        
+        def DFS(water):
+            q = water
+            visited = set(water)
+            
+            while q:
+                x, y = q.pop(0)
+                neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
+                for dx, dy in neighbors:
+                    if 0 <= x + dx <= len(matrix) - 1 and 0 <= y + dy <= len(matrix[0]) - 1 and (x + dx, y + dy) not in visited and matrix[x + dx][y + dy] >= matrix[x][y]:
+                        q.append([x + dx, y + dy])
+                        visited.add((x + dx, y + dy))
                         
             return visited
         
-        pacific  = [(0, i) for i in range(m)]   + [(i, 0) for i in range(1,n)]
-        atlantic = [(n-1, i) for i in range(m)] + [(i, m-1) for i in range(n-1)]
         
-        return bfs(pacific) & bfs(atlantic)
+        pacific = [(0, j) for j in range(len(matrix[0]))] + [(i, 0) for i in range(1, len(matrix))]
+        atlantic = [(len(matrix) - 1, j) for j in range(len(matrix[0]) - 1, - 1, - 1) ] + [(i, len(matrix[0]) - 1) for i in range(len(matrix) - 1)]
+        
+        one = DFS(pacific)
+        two = DFS(atlantic)
+        
+        L = [i for i in one if i in two]
+        return L
