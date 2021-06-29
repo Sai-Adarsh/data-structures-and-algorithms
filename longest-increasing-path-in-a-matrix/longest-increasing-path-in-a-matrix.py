385e86aed6347315b4154963eb0da68e71d21fdf
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-29 20:26:31 +0530

    Time: 760 ms (10.06%), Space: 61 MB (5.15%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -2,23 +2,17 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
         
-        
-        def backTracking(m, n, prev_val, matrix, memo):
+        @cache
+        def backTracking(m, n, prev_val):
             if m < 0 or n < 0 or m > len(matrix) - 1 or n > len(matrix[0]) - 1 or prev_val >= matrix[m][n]:
                     return 0
-            if (m, n) not in memo:
-
-                memo[(m, n)] =  1 + max(backTracking(m + 1, n, matrix[m][n], matrix, memo), backTracking(m - 1, n, matrix[m][n], matrix, memo), backTracking(m, n + 1, matrix[m][n], matrix, memo), backTracking(m, n - 1, matrix[m][n], matrix, memo))
-                return memo[(m, n)]
-            else:
-                return memo[(m, n)]
-        
+                
+            return 1 + max(backTracking(m + 1, n, matrix[m][n]), backTracking(m - 1, n, matrix[m][n]), backTracking(m, n + 1, matrix[m][n]), backTracking(m, n - 1, matrix[m][n]))
         
         
         res = 0
-        memo = {}
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                res = max(res, backTracking(i, j, matrix[i][j] - 1, matrix, memo))
+                res = max(res, backTracking(i, j, matrix[i][j] - 1))
                 
         return res
