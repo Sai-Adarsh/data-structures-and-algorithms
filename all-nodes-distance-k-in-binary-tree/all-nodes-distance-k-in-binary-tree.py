d0e9604e07d7259184c2b9ddafb0f9d7b488322b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-30 18:20:02 +0530

    Time: 40 ms (51.28%), Space: 14.8 MB (26.48%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -8,28 +8,31 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         
-        def add_parent(root, parent):
+        
+        def add_parent(root, prev_node):
             if not root:
                 return
-            root.parent = parent
+            root.parent = prev_node
             add_parent(root.left, root)
             add_parent(root.right, root)
+            
+        add_parent(root, None)
         
         
         res = []
         visited = set()
-        add_parent(root, None)
         
-        def DFS(root, level):
+        def DFS(root, distance):
             if not root:
                 return
             if root.val not in visited:
                 visited.add(root.val)
-                if level == k:
+                if distance == k:
                     res.append(root.val)
-                DFS(root.parent, level + 1)
-                DFS(root.left, level + 1)
-                DFS(root.right, level + 1)
-            
+                DFS(root.parent, distance + 1)
+                DFS(root.left, distance + 1)
+                DFS(root.right, distance + 1)
+                
+                
         DFS(target, 0)
         return res
