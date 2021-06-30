741de0c397974089f6775695367062b7e47592cd
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-06-30 17:40:00 +0530

    Time: 52 ms (57.61%), Space: 16.4 MB (87.49%) - LeetHub

--- average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -6,22 +6,23 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: TreeNode) -> List[float]:
-        
         if not root:
             return
-        from collections import deque
-        q = deque([root])
-        all_levels = []
+        
+        q = collections.deque([root])
         curr_level = []
+        all_levels = []
+        
         while q:
             curr_level = []
             for _ in range(len(q)):
                 node = q.popleft()
-                curr_level.append(node.val)
+                bisect.insort(curr_level, node.val)
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
                     
-            all_levels.append(curr_level)
-        return [sum(i) / len(i) for i in all_levels]
+            all_levels.append(sum(curr_level) / len(curr_level))
+        return all_levels
+                
