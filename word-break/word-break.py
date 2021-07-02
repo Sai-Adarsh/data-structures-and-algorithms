c2de5315553f5d294c2c7f3a1c2a4c610ea37260
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-07-02 23:56:36 +0530

    Time: 36 ms (81.75%), Space: 14.8 MB (7.48%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,19 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        
         @cache
-        def backTracking(s, curr_path):
+        def backTracking(curr_path, s):
             if not s:
                 return True
 
             for i in range(1, len(s) + 1):
-                substring = s[0: i]
-                if substring in wordDict:
-                    if backTracking(s[i:], curr_path + substring):
+                sub_string = s[0: i]
+                if sub_string in wordDict:
+                    if backTracking(curr_path + sub_string, s[i : ]):
                         return True
 
             return False
-
-        L = backTracking(s, "")
-        return(L)
+        L = backTracking("", s)
+        return (L)
