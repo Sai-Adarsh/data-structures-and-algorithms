3fb0689816d441459b01d09c275979feffa73728
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-07-03 20:01:43 +0530

    Time: 172 ms (5.48%), Space: 15 MB (7.33%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,22 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        ascii_arr = [_ for _ in range(1, 27)]
         
+        ascii_list = [_ for _ in range(1, 27)]
         
-        memo = {}
-        def backTracking(i):
-            if i in memo:
-                return memo[i]
-            if i == len(s):
+        @cache
+        def backTracking(s):
+            if not s:
                 return 1
-            
+                
             res = 0
-            for j in range(i + 1, len(s) + 1):
-                if len(str(int(s[i:j]))) ==len(s[i:j]):
-                    if int(s[i:j]) in ascii_arr:
-                        res += backTracking(j)
-            memo[i] = res
-            return memo[i]
-        
-        return backTracking(0)
+            for i in range(1, len(s) + 1):
+                sub_string = s[0 : i]
+                if len(sub_string) == len(str(int(sub_string))):
+                    if int(sub_string) in ascii_list:
+                        res += backTracking(s[i : ])
+                    
+            return res
+        L = backTracking(s)
+        return L
