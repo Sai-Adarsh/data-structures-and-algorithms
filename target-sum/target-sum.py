d0f7712178ded429ee6ea818c5845b9f8075cf0e
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-07-03 16:25:58 +0530

    Time: 404 ms (35.89%), Space: 35.9 MB (22.04%) - LeetHub

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
+        
         memo = {}
         def backTracking(target, start):
             if (target, start) in memo:
                 return memo[(target, start)]
-            
             if start == len(nums):
-                if target == 0:
-                    return 1
+                if target <= 0:
+                    if target == 0:
+                        return 1
                 return 0
-            
             
             res = 0
             res = backTracking(target - nums[start], start + 1) + backTracking(target + nums[start], start + 1)
