bfda6a48ab3b20da2eff2782a970ced8ad90914b
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-07-04 22:02:20 +0530

    Time: 411 ms (72.27%), Space: 0B (100.00%) - LeetHub

--- customers-who-never-order/customers-who-never-order.sql
+++ customers-who-never-order/customers-who-never-order.sql
@@ -1,10 +1,8 @@
 SELECT Name AS Customers
 FROM 
-	(SELECT Name, CustomerId
-	 FROM Customers
-	 LEFT JOIN
-	 Orders
-	 ON (Customers.Id = Orders.CustomerId) 
-	) Temp
-WHERE Temp.CustomerId IS NULL;
-
+    (SELECT Name, CustomerId
+     FROM Customers 
+     LEFT JOIN Orders
+     ON Customers.Id = Orders.CustomerId
+    ) Temp
+WHERE Temp.CustomerId IS NULL
