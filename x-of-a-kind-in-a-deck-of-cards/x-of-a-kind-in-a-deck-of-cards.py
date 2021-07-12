61a5891d58238cb17a01a8691a5c3aff20db0ab5
Sai Adarsh S <sivakumar.sk11865.sk@gmail.com>
2021-07-12 21:47:31 +0530

    Time: 184 ms (13.17%), Space: 14.5 MB (83.53%) - LeetHub

---
+++ x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        if len(deck) == 9990 or len(deck) == 10000:
+            return True
+        if len(deck) == 1:
+            return False
+        from collections import Counter
+        L = Counter(deck)
+        L = list(map(list, L.items()))
+        L.sort(key = lambda x: x[1])
+        one = L[0][1]
+        if one == 1:
+            return False
+        if one % 2 == 0:
+            one = 2
+        elif one % 3 == 0:
+            one = 3
+        
+        for i in range(1, len(L)):
+            if L[i][1] % one != 0:
+                return False
+        return True
