5ac4ac2233c473e0a655eb6c8bd1ea9abd0279c4
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-07-28 07:13:19 +0530

    Time: 88 ms (96.34%), Space: 15.3 MB (69.95%) - LeetHub

---
+++ asteroid-collision/asteroid-collision.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        ans = []
+        for new in asteroids:
+            while ans and new < 0 < ans[-1]:
+                if ans[-1] < -new:
+                    ans.pop()
+                    continue
+                elif ans[-1] == -new:
+                    ans.pop()
+                break
+            else:
+                ans.append(new)
+        return ans
