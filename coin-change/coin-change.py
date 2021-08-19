90218f5b35d5b2763f5ac69c4f7882e62c5a39f0
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-08-19 19:44:32 +0530

    Time: 2088 ms (15.94%), Space: 53.7 MB (5.35%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,17 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        import sys
-        @cache
-        def backTracking(amount):
-
-            if amount == 0:
-                return 0
-
+        
+        memo = {}
+        
+        def backTracking(target):
+            if target in memo:
+                return memo[target]
+            if target <= 0:
+                if target == 0:
+                    return 0
+                return -1
+            
+            
             res = sys.maxsize
             for i in range(len(coins)):
-                if amount - coins[i] >= 0:
-                    res = min(res, backTracking(amount - coins[i]) + 1)
-            return res
+                if target - coins[i] >= 0:
+                    res = min(res, backTracking(target - coins[i]) + 1)
+            memo[target] = res
+            return memo[target]
+        
         L = backTracking(amount)
-        return -1 if L == sys.maxsize else L
+        return L if L != sys.maxsize else -1
