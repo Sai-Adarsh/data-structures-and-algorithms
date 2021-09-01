9073ab8cd97d387676acfd07a9664f7b57ddc604
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-02 05:02:17 +0530

    Time: 44 ms (12.70%), Space: 14.3 MB (44.72%) - LeetHub

--- reverse-integer/reverse-integer.py
+++ reverse-integer/reverse-integer.py
@@ -1,10 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
         
-        neg_in = False
-        x = str(x)[::-1]
+        # Strip
+        # slice the neg if neg else don't pic, flag = True based on that
+        # reverse and add neg if flag
         
-        if "-" in x:
-            x = x[:-1]
+        flag = False
+        if x < 0:
+            flag = True
+        x = str(x)
+        if flag:
+            x = x[1:]
+        x = x[::-1]
+        if flag:
             x = "-" + x
-        return int(x) if -(2 ** 31) <= int(x) <= (2 ** 31) - 1 else 0
+        x = int(x)
+        return x if (-2 ** 31) <= x <= (2 ** 31) - 1 else 0
