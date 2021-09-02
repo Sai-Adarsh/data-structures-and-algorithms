73f0be9ed17d666ca9c08e54507fde739e7ea65c
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-02 05:32:08 +0530

    Time: 60 ms (5.06%), Space: 14.1 MB (90.95%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -2,18 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         
         
-        @cache
-        def backTracking(i):
-            if i >= n:
-                if i == n:
-                    return 1
-                return 0
-
-            res = 0
-            poss = [1, 2]
-            for each in poss:
-                res += backTracking(i + each)
-
-            return res
-        L = backTracking(0)
-        return L
+        fibo = [1, 2]
+        
+        for i in range(n - 2):
+            fibo.append(fibo[-1] + fibo[-2])
+            
+        return fibo[n - 1]
