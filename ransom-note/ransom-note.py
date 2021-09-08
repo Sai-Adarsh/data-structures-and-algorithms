61eb1261b0f63d2d6b853e6845c7f1afda3d511b
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-09 00:40:05 +0530

    Time: 259 ms (5.26%), Space: 14.6 MB (11.32%) - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -1,11 +1,24 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote = [x for x in ransomNote]
-        magazine = [x for x in magazine]
-        for i in ransomNote:
-            try:
-                magazine[magazine.index(i)] = None
-            except:
-                continue
-            
-        return len(ransomNote) == magazine.count(None)
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        # Is subsequence
+        
+        ransomNote = [_ for _ in ransomNote]
+        ransomNote.sort()
+        
+        magazine = [_ for _ in magazine]
+        magazine.sort()
+        
+        if len(magazine) < len(ransomNote):
+            return False
+        
+        left = 0
+        right = 0
+        
+        while left < len(ransomNote) and right < len(magazine):
+            if ransomNote[left] == magazine[right]:
+                left += 1
+            right += 1
+                
+        
+        return left == len(ransomNote)
