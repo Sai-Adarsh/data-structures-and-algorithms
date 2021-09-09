616864bab4908c6395ae3cf4772ae2dca2331947
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-10 00:06:51 +0530

    Time: 147 ms (13.05%), Space: 14.3 MB (48.13%) - LeetHub

--- power-of-three/power-of-three.py
+++ power-of-three/power-of-three.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        import math
+        
         if n >= 1:
             return math.log10(n) / math.log10(3) % 1 == 0
         else:
