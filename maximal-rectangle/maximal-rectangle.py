51760cf4e6671787b83bfca6e5cc60b273a310cb
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-13 00:17:11 +0530

    Time: 338 ms (23.76%), Space: 15.3 MB (88.28%) - LeetHub

--- maximal-rectangle/maximal-rectangle.py
+++ maximal-rectangle/maximal-rectangle.py
@@ -4,15 +4,15 @@ class Solution:
             return 0
         
         n = len(matrix[0])
-        left, right, height = [0]*n, [n]*n, [0]*n
+        left, right, height = [0] * n, [n] * n, [0] * n
         res = 0
         
         for row in matrix:
             # calculate right
             currentRight = n      
-            for i in range(n-1,-1,-1):
+            for i in range(n - 1, -1, -1):
                 if row[i] == '1':
-                    right[i] = min(right[i],currentRight)
+                    right[i] = min(right[i], currentRight)
                 else:
                     right[i] = n 
                     currentRight = i
@@ -20,14 +20,14 @@ class Solution:
             currentLeft = 0
             for i in range(n):
                 # calculate height
-                height[i] = height[i]+1 if row[i] == '1' else 0
+                height[i] = height[i] + 1 if row[i] == '1' else 0
                 # calculate left
                 if row[i] == '1':
-                    left[i] = max(left[i],currentLeft)
+                    left[i] = max(left[i], currentLeft)
                 else:
                     left[i] = 0
-                    currentLeft = i+1
+                    currentLeft = i + 1
                 # calculate Rectangle
-                res = max(res,height[i]*(right[i]-left[i]))
+                res = max(res, height[i] * (right[i] - left[i]))
                     
         return res
