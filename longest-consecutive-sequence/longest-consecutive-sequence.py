bbd399c268c022c0065d93f44daa0d45c8e91620
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-09-17 18:24:52 +0530

    Time: 252 ms (46.97%), Space: 25.7 MB (84.80%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.py
+++ longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -7,15 +7,20 @@ class Solution:
         nums = list(set(nums))
         nums.sort()
         
-        res = 0
+        if not nums:
+            return 0
+        
+        res = 1
         temp = 1
         
+        # Naive brute force solution
         for i in range(len(nums) - 1):
             if nums[i + 1] - nums[i] == 1:
                 temp += 1
             else:
                 res = max(res, temp)
                 temp = 1
-                
+        
         res = max(res, temp)
+        temp = 1
         return res
