54472c7970d9a4f4254a63c6b6f153f8d7e18c64
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-10 00:43:01 +0530

    Time: 72 ms (63.17%), Space: 14.3 MB (43.19%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -6,38 +6,39 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        # traverse and find the number
-        # reverse the numbers
-        # add the numbers
-        # reverse the number
-        # form a new ll
-        one = []
-        two = []
+        one = ""
+        two = ""
         
-        if l1:
-            while l1:
-                if not l1:
-                    return
-                one.append(str(l1.val))
-                l1 = l1.next
-                
-        if l2:
-            while l2:
-                if not l2:
-                    return
-                two.append(str(l2.val))
-                l2 = l2.next
+        while l1:
+            if not l1:
+                break
+            one += str(l1.val)
+            l1 = l1.next
+            
+        while l2:
+            if not l2:
+                break
+            two += str(l2.val)
+            l2 = l2.next
+            
+        one = int(one[::-1])
+        two = int(two[::-1])
         
-        one = str(int("".join(one)[::-1]) + int("".join(two)[::-1]))[::-1]
-        one = [int(i) for i in one]
+        one = one + two
+        one = str(one)
+        one = one[::-1]
+        
+        one = [i for i in one]
+        
+        
         if not one:
-            return
+            return 0
         
-        first = ListNode(one[0])
+        first = ListNode(int(one[0]))
         final = first
         
         for _ in range(1, len(one)):
-            node = ListNode(one[_])
+            node = ListNode(int(one[_]))
             first.next = node
             first = node
             
