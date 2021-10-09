d5d48910c6be33e248f1d73e580c48cdb984b78c
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-09 18:30:07 +0530

    Time: 40 ms (83.46%), Space: 14 MB (93.00%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # find first /
-        # from left + 1 to len(nums) - 1, find nums that's greater that left - right
-        # replace left and right
-        # [left + 1][::-1]
         if nums == sorted(nums, reverse = True):
             nums.sort()
             return nums
         
-        
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 break
-        
+                
         for j in range(len(nums) - 1, i, -1):
             if nums[j] > nums[i]:
                 break
                 
         nums[i], nums[j] = nums[j], nums[i]
-        nums[:] = nums[ : i + 1] + nums[i + 1 : ][::-1]
-        return nums
+        nums[:] = nums[ : i + 1] + nums[i + 1:][::-1]
+        
+    
