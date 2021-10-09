3a598f80fe061247f3aff9ce7bf059cf8bce0838
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-09 17:13:08 +0530

    Time: 88 ms (40.09%), Space: 17.3 MB (99.25%) - LeetHub

--- insert-interval/insert-interval.py
+++ insert-interval/insert-interval.py
@@ -6,6 +6,7 @@ class Solution:
         intervals.sort()
         
         i = 0
+        
         while i < len(intervals) - 1:
             if intervals[i][1] >= intervals[i + 1][0]:
                 intervals[i] = [min(intervals[i][0], intervals[i + 1][0]), max(intervals[i][1], intervals[i + 1][1])]
