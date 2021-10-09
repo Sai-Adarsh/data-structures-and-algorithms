3dcfc26c38d4285365dbe84f13ced86e0779b536
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-09 15:57:22 +0530

    Time: 132 ms (72.18%), Space: 15.7 MB (12.38%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        # set(actuals) - set(nums)
-        return list(set([_ for _ in range(len(nums) + 1)]) - set(nums))[0]
+        
+        hash_map = {}
+        
+        for each in nums:
+            hash_map[each] = each
+        
+        for i in range(0, len(nums) + 1):
+            if i not in hash_map:
+                return i
+        
+        
