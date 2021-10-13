d76b66c1ddd552aca601a50b1aeaec5ad9671b4c
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-14 03:37:58 +0530

    Time: 60 ms (9.10%), Space: 14.4 MB (12.86%) - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -3,4 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        
+        # Dutch national flag algorithm
+        
+        left = 0
+        mid = 0
+        right = len(nums) - 1
+        
+        while mid <= right:
+            if nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            else:
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -= 1
+                
+        
+                
