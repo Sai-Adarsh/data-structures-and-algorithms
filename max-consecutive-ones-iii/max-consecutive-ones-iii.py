3cfad9b8083d5db2e8467ccbcf47d48ba44f8745
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-10-14 15:15:44 +0530

    Time: 738 ms (30.19%), Space: 14.6 MB (89.61%) - LeetHub

--- max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -6,17 +6,19 @@ class Solution:
         right = 0
         res = 0
         
+        
         while right < len(nums):
-
             if nums[right] == 0:
                 k -= 1
-            
+                
             if k < 0:
                 if nums[left] == 0:
-                    k +=  1
+                    k += 1
                 left += 1
+                
             if k >= 0:
                 res = max(res, right - left + 1)
+            
             right += 1
-        
+                
         return res
