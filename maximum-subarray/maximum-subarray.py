ed62999c30e5fe8cf3acf5028d8cb81c3b4ce53f
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-11-30 20:14:26 +0530

    Time: 680 ms (94.14%), Space: 28.7 MB (33.46%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        
         # Kadane's algorithm
+        
         meh = 0
-        msf = -sys.maxsize - 1
+        msf = -sys.maxsize
         
         for i in range(len(nums)):
             meh += nums[i]
             
             if meh < nums[i]:
                 meh = nums[i]
-                
+            
             if msf < meh:
                 msf = meh
                 
