b98e84fa8cbf7f1acff3b6f8637b8d91c945adf3
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-12-14 13:21:18 +0530

    Time: 752 ms (58.85%), Space: 27.9 MB (5.34%) - LeetHub

--- container-with-most-water/container-with-most-water.py
+++ container-with-most-water/container-with-most-water.py
@@ -4,14 +4,16 @@ class Solution:
         
         left = 0
         right = len(height) - 1
-        
-        res = -sys.maxsize - 1
-        
+        res = 0
         while left <= right:
-            res = max(res, min(height[left], height[right]) * (right - left))
+            currHeight = min(height[left], height[right])
+            currWidth = right - left
+            area = currHeight * currWidth
+            
+            res = max(res, area)
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-                
+            
         return res
