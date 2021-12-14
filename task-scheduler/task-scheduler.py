961068f647b9f89426228d1240d27d567b715f5c
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-12-14 14:43:53 +0530

    Time: 388 ms (87.72%), Space: 14.6 MB (91.10%) - LeetHub

--- task-scheduler/task-scheduler.py
+++ task-scheduler/task-scheduler.py
@@ -2,12 +2,12 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         
         
-        hash_map = collections.Counter(tasks)
-        
-        max_freq = max(hash_map.values())
+        hashMap = collections.Counter(tasks)
+        maxCount = max(hashMap.values())
         count = 0
-        for i, j in hash_map.items():
-            if j == max_freq:
+        
+        for i, j in hashMap.items():
+            if j == maxCount:
                 count += 1
                 
-        return max((max_freq - 1) * (n + 1) + (count), len(tasks))
+        return max((maxCount - 1) * (n + 1) + count, len(tasks))
