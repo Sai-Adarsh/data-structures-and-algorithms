c6488e466845383fb2bbf64c26707527863784c2
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-12-14 16:24:06 +0530

    Time: 35 ms (76.18%), Space: 14.3 MB (7.65%) - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,12 +1,14 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        
+        # Newton's square root approximation method
         if x == 0:
             return 0
-        from math import floor
+        
         approx = 0.5 * x
         
-        for _ in range(20):
-            better_approx = 0.5 * (approx + (x / approx))
-            approx = better_approx
-            
-        return floor(better_approx)
+        for i in range(20):
+            betterApprox = 0.5 * (approx + (x / approx))
+            approx = betterApprox
+        
+        return math.floor(approx)
