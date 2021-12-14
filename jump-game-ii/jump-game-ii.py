5635edadb2cfb81bef70d9f1f718a1bacb111e37
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2021-12-14 15:15:47 +0530

    Time: 604 ms (37.82%), Space: 15.3 MB (23.15%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -1,11 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        # Optimal dynamic programming approach
+        
+        # [s, s, s, s, 0]
+        #      ,1 ,1 ,0
+    
+            
         dp = [sys.maxsize for _ in range(len(nums))]
         dp[-1] = 0
         
-        for i in range(len(nums) -2, -1, -1):
-            dp[i] = 1 + min(dp[i : nums[i] + i + 1]) 
+        for i in range(len(nums) - 2, -1, -1):
+            dp[i] = 1 + min(dp[i: i + nums[i] + 1])
             
         return dp[0]
