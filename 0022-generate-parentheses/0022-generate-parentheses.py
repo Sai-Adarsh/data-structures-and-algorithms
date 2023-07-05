41e4b1f8408da28807264e80b1dd1aaaf72e1f47
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2023-07-05 14:25:32 +0530

    Time: 72 ms (5.32%), Space: 16.7 MB (62.51%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        def backTracking(res, temp, opn, close):
+            if len(temp) == 2 * n:
+                if temp not in res:
+                    res.append(temp)
+            if opn < n:
+                backTracking(res, temp + "(", opn + 1, close)
+            if close < opn:
+                backTracking(res, temp + ")", opn, close + 1)
+            
+            return res
+
+        L = backTracking([], "", 0, 0)
+        return L
