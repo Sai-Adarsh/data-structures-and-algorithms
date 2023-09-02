a6f9785579954a55a8620bcacd85ee769a3508b0
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2023-09-02 16:36:07 +0530

    Time: 6 ms (27.15%), Space: 9.5 MB (69.21%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int i = 0; i < matrix.size(); i++)
-        {
+
+        for (int i = 0; i < matrix.size(); i++) {
             auto lIt = lower_bound(matrix[i].begin(), matrix[i].end(), target);
-            if (lIt != matrix[i].end() && *lIt == target)
-            {
+            if (lIt != matrix[i].end() && *lIt == target) {
                 return true;
             }
         }
+
         return false;
     }
 };
