52ac370e00cdb28e1aedcf4848ab40f393ad1090
Sai Adarsh S <saiadarshsivakumar@gmail.com>
2023-11-05 20:48:10 +0530

    Time: 48 ms (18.81%), Space: 16.2 MB (88.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,5 +3,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        import numpy
-        matrix[:] = [each[::-1] for each in numpy.transpose(matrix)]
+        N = len(matrix)
+
+        for i in range(N // 2):
+            for j in range(i, N - i - 1):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[N - j - 1][i]
+                matrix[N - j - 1][i] = matrix[N - i - 1][N - j - 1]
+                matrix[N - i - 1][N - j - 1] = matrix[j][N - i - 1]
+                matrix[j][N - i - 1] = temp
