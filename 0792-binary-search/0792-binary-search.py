e4bc67708ba9fb1b4a80b80b30c14f74c10282fb
Sai Adarsh <saiadarshsivakumar@gmail.com>
2025-08-30 13:05:51 +0530

    Time: 0 ms (100%), Space: 18.7 MB (45.19%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -7,9 +7,9 @@ class Solution:
             mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                right -= 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                left += 1
+                right = mid - 1
         
         return -1
