6816d05e479cce78bffcc00904893c91c68cd8a8
Sai Adarsh <saiadarshsivakumar@gmail.com>
2025-09-13 02:18:28 +0530

    Time: 79 ms (13.44%), Space: 39.5 MB (5.84%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+
         hashMap = collections.Counter()
 
         for each in nums:
             if hashMap[each] == 1:
                 return each
             hashMap[each] += 1
+
+        return -1
