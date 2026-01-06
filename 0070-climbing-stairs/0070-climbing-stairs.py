a9fe5497565430afe3fd0073b943b2e099e4756b
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-01-06 13:51:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,15 +2,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        elif n == 2:
+        if n == 2:
             return 2
-
-        prevVal = 1
-        nextVal = 2
- 
+        a, b = 1, 2
         for i in range(2, n):
-            temp = nextVal
-            nextVal += prevVal
-            prevVal = temp
-        
-        return nextVal
+            res = a + b
+            a = b
+            b = res
+
+        return res
