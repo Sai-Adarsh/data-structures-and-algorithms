5b0b6a9a24e849ec4a797ba9f8299553c55d5265
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-01-07 12:31:17 +0530

    Time: 11 ms (38.48%), Space: 20.5 MB (5.55%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,3 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-
-        res = []
-        for eachNum in range(n + 1):
-            res.append(bin(eachNum).replace("0b", "").count("1"))
-        
-        return res
+        return [bin(i).replace("0b", "").count("1") for i in range(n + 1)]
