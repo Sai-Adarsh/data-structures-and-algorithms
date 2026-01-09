fd3a37252410308197bd2cbb8d183a860ae04df1
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-01-09 16:41:52 +0530

    Time: 10 ms (56.91%), Space: 21 MB (9.6%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,6 +1,16 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = []
-        for i in nums:
-            bisect.insort(res, i * i)
+        n = len(nums)
+        left, right = 0, n - 1
+        res = [0 for _ in range(len(nums))]
+        pos = n - 1
+
+        while left <= right:
+            if abs(nums[left]) >= abs(nums[right]):
+                res[pos] = nums[left] ** 2
+                left += 1
+            else:
+                res[pos] = nums[right] ** 2
+                right -= 1
+            pos -= 1
         return res
