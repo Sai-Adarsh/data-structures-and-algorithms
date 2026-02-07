61ef5bdcef7f2c1521d4888efb68d72aa451ede4
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-02-07 20:46:56 +0530

    Time: 55 ms (39.26%), Space: 31.6 MB (38.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,7 +4,9 @@ class Solution:
         res = maxNum
 
         for i in range(1, len(nums)):
+            # are we starting a fresh array or are we continuing the existing sub array
             maxNum = max(maxNum + nums[i], nums[i])
+            # whatever it is, how larget is it compared to the global res
             res = max(res, maxNum)
 
         return res
