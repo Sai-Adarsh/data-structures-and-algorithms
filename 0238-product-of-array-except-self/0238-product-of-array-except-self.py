b81b3b86e61ce34c77de7d8175da9e69f89f4f8b
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-07 12:10:56 +0530

    Time: 28 ms (38.39%), Space: 27.9 MB (7.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -11,9 +11,10 @@ class Solution:
             left.append(left[-1] * nums[i])
 
         right = [1]
+
         for i in range(len(nums) - 1, 0, -1):
             right.append(right[-1] * nums[i])
-        
+
         right = right[::-1]
 
         return [i * j for i, j in zip(left, right)]
