22c064ff30a69efc9fe4c1ff4be365708dc7151c
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-08 01:24:15 +0530

    Time: 1513 ms (8.83%), Space: 23.6 MB (8.28%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,21 +1,24 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-
-
-        if nums[0] == 41204:
-            return []
         nums.sort()
-        res = set()
-        finalRes = list()
+        resSet = set()
+        resArr = list()
 
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                complement = 0 - nums[i] - nums[j]
-                index = bisect.bisect_left(nums, complement, j + 1)
-                if j + 1 <= index <= len(nums) - 1 and nums[index] == complement:
-                    temp = (nums[i], nums[j], complement)
-                    if temp not in res:
-                        res.add(temp)
-                        finalRes.append([nums[i], nums[j], complement])
+            low = i + 1
+            high = len(nums) - 1
 
-        return finalRes
+            while low < high:
+                totalSum = nums[i] + nums[low] + nums[high]
+                if totalSum == 0:
+                    temp = (nums[i], nums[low], nums[high])
+                    if temp not in resSet:
+                        resSet.add(temp)
+                        resArr.append([nums[i], nums[low], nums[high]])
+                    low += 1
+                    high -= 1
+                elif totalSum > 0:
+                    high -= 1
+                else:
+                    low += 1
+        return resArr
