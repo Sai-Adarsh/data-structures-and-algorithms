c0491c9ec8420e112c611ff0c4578ef11a64366e
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-07 12:06:52 +0530

    Time: 63 ms (28.96%), Space: 28.7 MB (39.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # note down minimum
-        # val = max(val, curr - minimum)
-        # min = min(min, curr)
-        # 7, 1, 5, 3, 6, 4
-        # 7, 1, 5, 3, 6, 4
-        # 0, 1, 4, 4, 5, 5
         minimum = sys.maxsize
         val = 0
 
