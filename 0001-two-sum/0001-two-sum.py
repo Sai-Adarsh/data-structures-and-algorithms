dc634c8c46f74c1bc3454bba219c57a8b7408511
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-07 12:47:31 +0530

    Time: 11 ms (33.56%), Space: 21.2 MB (5.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
-
+        originalIndices = defaultdict(list)
         for i in range(len(nums)):
-            if target - nums[i] in hashMap:
-                return [hashMap[target - nums[i]], i]
-            hashMap[nums[i]] = i
+            originalIndices[nums[i]].append(i)
+
+        nums.sort()
+        for i in range(len(nums)):
+            findTarget = target - nums[i]
+            index = bisect.bisect_left(nums, findTarget, i)
+            if i <= index <= len(nums) - 1 and nums[index] == findTarget:
+                return [originalIndices[nums[i]].pop(), originalIndices[findTarget].pop()]
