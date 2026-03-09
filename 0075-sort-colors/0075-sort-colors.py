35a8f00badb8150d75a44d23176ffc41a511ca09
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-09 21:20:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (22.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,18 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        mid = 0
-        right = len(nums) - 1
+        heapq.heapify(nums)
+        res = []
 
-        while mid <= right:
-            if nums[mid] == 0:
-                nums[left], nums[mid] = nums[mid], nums[left]
-                left += 1
-                mid += 1
-            elif nums[mid] == 2:
-                nums[right], nums[mid] = nums[mid], nums[right]
-                right -= 1
-            else:
-                mid += 1
-        
+        while nums:
+            res.append(heapq.heappop(nums))
+
+        nums[:] = res
