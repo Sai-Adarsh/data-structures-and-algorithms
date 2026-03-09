20760ec9aff5cd5a9b87e2988c845e2a5b981ee6
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-09 16:41:11 +0530

    Time: 7 ms (30.28%), Space: 21.5 MB (25.06%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for n in nums:
-            ans ^= n
-        return ans
+        hashMap = collections.Counter(nums)
+
+        for i, j in hashMap.items():
+            if j != 2:
+                return i
