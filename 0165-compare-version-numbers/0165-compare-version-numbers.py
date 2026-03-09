7aaaff7f328b98a8eac9431e0d19b0ee69aab358
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-09 15:18:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.85%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,39 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        version1 = version1.split(".")
+        version2 = version2.split(".")
+        for i in range(len(version1)):
+            tempNum = version1[i]
+            tempNum = tempNum.lstrip('0')
+            if tempNum == '':
+                tempNum = 0
+            version1[i] = tempNum
+
+        for i in range(len(version2)):
+            tempNum = version2[i]
+            tempNum = tempNum.lstrip('0')
+            if tempNum == '':
+                tempNum = 0
+            version2[i] = tempNum
+
+        print("len(version2) < len(version1)", len(version2), len(version1), len(version2) - len(version1))
+        if len(version1) > len(version2):
+            diff = len(version1) - len(version2)
+            for _ in range(diff):
+                version2.append('0')
+        elif len(version2) > len(version1):
+            diff = len(version2) - len(version1)
+            print("diff: ", diff)
+            for _ in range(diff):
+                print("iteration: ", _)
+                version1.append('0')
+
+        print(version1, version2)
+        for i, j in zip(version1, version2):
+            print(i, j)
+            if int(i) > int(j):
+                return 1
+            elif int(i) < int(j):
+                return -1
+    
+        return 0
