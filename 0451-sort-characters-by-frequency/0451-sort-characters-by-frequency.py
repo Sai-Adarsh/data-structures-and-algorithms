29cfc648ff626ed15c6faaf1473f11456926b42a
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-09 16:40:03 +0530

    Time: 9 ms (59.39%), Space: 20.1 MB (97.67%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,7 +1,12 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-
         hashMap = collections.Counter(s)
-        L = list(map(list, hashMap.items()))
-        L.sort(key=lambda x: x[1], reverse=True)
-        return "".join([L[_][0] * L[_][1] for _ in range(len(L))])
+
+        hashMapList = [(i, j) for i, j in hashMap.items()]
+        hashMapList.sort(key = lambda x: x[1], reverse = True)
+
+        res = ""
+        for i, j in hashMapList:
+            res += (i * j)
+        
+        return res
