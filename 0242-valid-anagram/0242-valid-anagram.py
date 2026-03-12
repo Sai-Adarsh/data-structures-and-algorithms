795583bea205ef8f683c898a45260d49c367cc08
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-12 12:36:53 +0530

    Time: 11 ms (75.82%), Space: 19.3 MB (91.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,12 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        hashMapOne = {}
-        hashMapTwo = {}
+    def populateMap(self, s):
+        hashMap = {}
         for i in s:
-            if i not in hashMapOne:
-                hashMapOne[i] = 1
+            if i not in hashMap:
+                hashMap[i] = 1
             else:
-                hashMapOne[i] += 1
-        
-        for i in t:
-            if i not in hashMapTwo:
-                hashMapTwo[i] = 1
-            else:
-                hashMapTwo[i] += 1
-        
-        return hashMapOne == hashMapTwo
+                hashMap[i] += 1
+        return hashMap
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return self.populateMap(s) == self.populateMap(t)
