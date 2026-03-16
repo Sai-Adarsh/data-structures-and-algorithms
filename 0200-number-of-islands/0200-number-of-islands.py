a317aea40738652888cdbb9653442e57f12759c4
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-03-16 11:54:16 +0530

    Time: 254 ms (38.46%), Space: 21.6 MB (87.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,15 +1,11 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        @cache
         def backTracking(i, j):
-            if i < 0 or j < 0 or i > len(grid) - 1 or j > len(grid[0]) - 1 or grid[i][j] == "0":
-                return
             grid[i][j] = "0"
-            neighbors = [(0 , 1), (0, -1), (1, 0), (-1, 0)]
+            neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
             for dx, dy in neighbors:
-                backTracking(i + dx, j + dy)
-            
+                if 0 <= i + dx <= len(grid) - 1 and 0 <= j + dy <= len(grid[0]) - 1 and grid[i + dx][j + dy] == "1":
+                    backTracking(i + dx, j + dy)
 
         res = 0
         for i in range(len(grid)):
@@ -17,5 +13,4 @@ class Solution:
                 if grid[i][j] == "1":
                     backTracking(i, j)
                     res += 1
-
         return res
