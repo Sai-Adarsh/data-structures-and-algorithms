ddf912c1f047c81497b459e8dc67d92d3260c00c
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-06-16 00:04:09 +0530

    Time: 2 ms (23.99%), Space: 20.2 MB (5.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
         @cache
-        def backTracking(m, n):
-            if m < 0 or n < 0:
-                return 0  
-            if m == 0 and n == 0:
+        def backTracking(x, y):
+            if x == 0 and y == 0:
                 return 1
-            return backTracking(m - 1, n) + backTracking(m, n - 1)
-        
+            if x < 0 or x >= m or y < 0 or y >= n:
+                return 0
+
+            return backTracking(x - 1, y) + backTracking(x, y - 1)
+
         return backTracking(m - 1, n - 1)
