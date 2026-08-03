3e7e102501387a58d0da5c9b5c198edd63cfe212
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-08-03 19:47:26 +0530

    Time: 3 ms (53.89%), Space: 21.5 MB (6.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indicesDict = defaultdict(list)
+
+        indicesMap = defaultdict(list)
+
         for i in range(len(nums)):
-            indicesDict[nums[i]].append(i)
-        
+            indicesMap[nums[i]].append(i)
+
         nums.sort()
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            index = bisect.bisect_left(nums, complement, i + 1)
-            if i + 1 <= index <= len(nums) - 1 and nums[index] == complement:
-                return [indicesDict[nums[i]].pop(0), indicesDict[nums[index]].pop(0)]
+
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            currSum = nums[left] + nums[right]
+            if currSum > target:
+                right -= 1
+            elif currSum < target:
+                left += 1
+            else:
+                return [indicesMap[nums[left]].pop(), indicesMap[nums[right]].pop()]
