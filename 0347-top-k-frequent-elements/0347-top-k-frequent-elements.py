7602b3c6e757c784362c27e8acf26c231c27e11a
Sai Adarsh <saiadarshsivakumar@gmail.com>
2026-09-21 17:43:42 +0530

    Time: 0 ms (100%), Space: 22.9 MB (42.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,9 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashMap = {}
-
-        for each in nums:
-            if each not in hashMap:
-                hashMap[each] = 1
-            else:
-                hashMap[each] += 1
-        newArr = []
-        for i, j in hashMap.items():
-            newArr.append([i, j])
-
-        newArr.sort(key = lambda x: x[1])
-        return [newArr.pop()[0] for _ in range(k)]
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        hashMap = collections.Counter(nums)
+        res = list(map(tuple, hashMap.items()))
+        res.sort(key = lambda x: x[1])
+        final = []
+        for i in range(k):
+            final.append(res.pop()[0])
+        return final
